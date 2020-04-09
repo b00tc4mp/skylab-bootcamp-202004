9c772f02648a0062b0aed0a8b838659c5d833c8b
mac <mac@MacBook-Pro-de-mac.local>
2020-04-09 09:39:02 +0200

    apply changes js file

--- staff/sergi-ramos/html-semantics/extract-html-semantics.js
+++ staff/sergi-ramos/html-semantics/extract-html-semantics.js
@@ -1,16 +1,66 @@
-var semantic = [];
+var htmlString = document.getElementsByTagName("html")[0].innerHTML;
+// Faltaba añadir \ para que detectara todo el regex
+var regex = /<(([a-z])+[0-9]*(\ |>))|(<\/[a-z]+>)/g;
+var regexTags = htmlString.match(regex);
+var tagList = [
+  "<head>",
+  "</head>",
+  "<article>",
+  "</article>",
+  "<audio>",
+  "</audio>",
+  "<button>",
+  "</button>",
+  "<form>",
+  "</form>",
+  "<video>",
+  "</video>",
+  "<aside>",
+  "</aside>",
+  "<details>",
+  "</details>",
+  "<figcaption>",
+  "</figcaption>",
+  "<figure>",
+  "</figure>",
+  "<footer>",
+  "</footer>",
+  "<header>",
+  "</header>",
+  "<img>",
+  "<main>",
+  "</main>",
+  "<mark>",
+  "</mark>",
+  "<nav>",
+  "</nav>",
+  "<section>",
+  "</section>",
+  "<summary>",
+  "</summary>",
+  "<time>",
+  "</time>",
+  "</summary>",
+];
 
-function extractHtml(tag) {
-    debugger
-    for (var i = 0; i < document.childNodes.length; i++) {
-        if (typeof (document.childNodes[i].tagName) == 'string') {
-            semantic.push(document.childNodes[i].tagName);
-            if (document.childNodes[i].hasChildNodes) {
-                for(var x = 0;document.childNodes[i].length; x++){
-                extractHtml(document.childNodes[i])
-                }
-            }
-        }
-
-    }
+for (var i = 0; i < regexTags.length; i++) {
+  if (regexTags[i].indexOf(" ") !== -1) {
+    regexTags[i] = regexTags[i].slice(0, -1);
+    regexTags[i] += ">";
+  }
 }
+
+regexTags.filter(function (tag) {
+  if (tagList.includes(tag)) {
+    return tag;
+  }
+});
+
+var semanticTags = [];
+for (var j = 0; j < regexTags.length; j++) {
+  if (tagList.includes(regexTags[j])) {
+    semanticTags.push(regexTags[j]);
+  }
+}
+
+htmlpage = semanticTags.join("");
