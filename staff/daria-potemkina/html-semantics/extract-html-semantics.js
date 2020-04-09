fd7a9b7264a19fdbda94c779592d1ebdd0395d78
Dashkent <dariapotemkina@mail.ru>
2020-04-09 09:57:42 +0200

    modify semantics

--- staff/daria-potemkina/html-semantics/extract-html-semantics.js
+++ staff/daria-potemkina/html-semantics/extract-html-semantics.js
@@ -1,14 +1,16 @@
 var semantics = []
+var semanticsTagClosed = ['BODY', 'HTML', 'HEAD', "ARTICLE", "AUDIO", "BUTTON", "FORM", "VIDEO", "ASIDE", "DETAILS", "FIGCAPTION", "FIGURE", "FOOTER", "HEADER", "IMG", "MAIN", "MARK", "NAV", "SECTION", "SUMMARY","TIME"];
+var semanticsTag = ['BODY', 'HTML', 'HEAD', "ARTICLE", "AUDIO", "BUTTON", "FORM", "VIDEO", "ASIDE", "DETAILS", "FIGCAPTION", "FIGURE", "FOOTER", "HEADER", "MAIN", "MARK", "NAV", "SECTION", "SUMMARY","TIME"];
 
 function semantic(tag) {
-    if(tag.tagName == 'BODY' || tag.tagName == 'HTML'|| tag.tagName == 'HEAD' || tag.tagName == "ARTICLE" || tag.tagName == "AUDIO" || tag.tagName == "BUTTON" || tag.tagName == "FORM" || tag.tagName == "VIDEO" || tag.tagName == "ASIDE" || tag.tagName == "DETAILS" || tag.tagName == "FIGCAPTION" || tag.tagName == "FIGURE" || tag.tagName == "FOOTER" || tag.tagName == "HEADER" ||  tag.tagName == "IMG" || tag.tagName == "MAIN" || tag.tagName == "MARK" || tag.tagName == "NAV" || tag.tagName == "SECTION" || tag.tagName == "SUMMARY" || tag.tagName == "TIME"){
-        semantics.push('<' + tag.tagName.toLowerCase() + '>')  
+    if(semanticsTagClosed.includes(tag.tagName)){
+        semantics.push(`\n <${tag.tagName.toLowerCase()}>`)  
   }  
     for (var element of tag.children) {
         semantic(element)
     } 
-     if (tag.tagName == 'BODY' || tag.tagName == 'HTML' || tag.tagName == 'HEAD' || tag.tagName == "ARTICLE" || tag.tagName == "AUDIO" || tag.tagName == "BUTTON" || tag.tagName == "FORM" || tag.tagName == "VIDEO" || tag.tagName == "ASIDE" || tag.tagName == "DETAILS" || tag.tagName == "FIGCAPTION" || tag.tagName == "FIGURE" || tag.tagName == "FOOTER" || tag.tagName == "HEADER" || tag.tagName == "MAIN" || tag.tagName == "MARK" || tag.tagName == "NAV" || tag.tagName == "SECTION" || tag.tagName == "SUMMARY" || tag.tagName == "TIME") {
-        semantics.push('</' + tag.tagName.toLowerCase() + '>');
+    if(semanticsTag.includes(tag.tagName)){
+        semantics.push(`</${tag.tagName.toLowerCase()}>\n`);
     }
     return semantics.join("");
 }
