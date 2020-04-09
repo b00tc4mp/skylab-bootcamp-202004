0e1a9a38e445b949ab6261beede92a539d18bb31
pauatro <pauatro@gmail.com>
2020-04-09 09:42:15 +0200

    html semantics exercise

--- staff/pau-atienza/html-semantics/extract-html-semantics.js
+++ staff/pau-atienza/html-semantics/extract-html-semantics.js
@@ -1,182 +1,104 @@
-// var semantic=[ 
-// "article",
-// "aside",
-// "details",
-// "figcaption",
-// "figure",
-// "footer",
-// "header",
-// "main",
-// "mark",
-// "nav",
-// "section",
-// "summary",
-// "time"];
+//Los elementos que hemos definido como semanticos
+var semantic=["ARTICLE",
+"BODY",
+"HTML",
+"IMG",
+"ASIDE",
+"DETAILS",
+"FIGCAPTION",
+"FIGURE",
+"FOOTER",
+"HEADER",
+"HEAD",
+"MAIN",
+"MARK",
+"NAV",
+"SECTION",
+"SUMMARY",
+"TIME"]
 
-var semantic=[ 
-    "ARTICLE",
-    "ASIDE",
-    "DETAILS",
-    "FIGCAPTION",
-    "FIGURE",
-    "FOOTER",
-    "HEADER",
-    "MAIN",
-    "MARK",
-    "NAV",
-    "SECTION",
-    "SUMMARY",
-    "TIME"
-];
+//los atributos que hay que borrar de los elementos
+var atribute=["itemscope",
+"itemtype",
+"class",
+"id",
+"alt",
+"data-src",
+"src",
+"loading",
+"style",
+"width",
+"data-sizes",
+"data-srcset",
+"data-maxdpr",
+"sizes",
+"srcset",
+"data-parallax-background-speed",
+"role",
+"itemprop",
+"datetime",
+"lang"]
 
-var exceptions= [ 
-    "HTML", 
-    "HEAD", 
-    "BODY"
-];
+//Mete todos los elementos de un documento en un mismo array
+var elements=document.getElementsByTagName("*");
 
-var elements = document.getElementsByTagName('*');
-
-function isSemantic(object, semantic){
-    for (var i= 0; i<semantic.length; i++){
-        if(object.tagName === semantic[i] ){
+//Comprueba si el elemento está dentro de los semanticos
+function isSemantic(ele) {
+    for(var i=0;i<semantic.length;i++){
+        if(ele.tagName==semantic[i] && ele.tagName!=null){
             return true;
         }
     }
     return false;
-};
+}
 
-function isException(object, exceptions){
-    for (var i= 0; i<exceptions.length; i++){
-        if(object.tagName === exceptions[i] ){
-            return true;
+//Ordena a los elementos que se muevan en la jerarquía
+function classify() {
+    
+    for(var i=elements.length-1;i>=0;i--){
+        moveToSemantic(elements[i]);
+    }
+}
+
+//Mueve el elemento hasta que es hijo de un elemento semantico
+function moveToSemantic(ele) {
+    if(ele.tagName!="HTML")
+    {
+        var oldEle=ele.parentElement;
+        if(!isSemantic(oldEle)){
+            oldEle.parentElement.appendChild(ele);
+            moveToSemantic(ele);
         }
     }
-    return false;
-};
+}
 
-// for (var i=0; i<elements.length; i++){
-//     if(isSemantic(elements[i], semantic)){
-//         elements[i].removeAttribute("class");
-//         elements[i].removeAttribute("id");
-//         elements[i].removeAttribute("style");
-//         elements[i].removeAttribute("lang");
-//         elements[i].innerText = '';
-//     }
-//     else if(isException(elements[i], exceptions)){
-//         elements[i].removeAttribute("class");
-//         elements[i].removeAttribute("id");
-//         elements[i].removeAttribute("style");
-//         elements[i].removeAttribute("lang");
-        
-//     }
-//     else{ 
-//         elementToEliminate = document.getElementsByTagName(elements[i].tagName)[0];
-  
-//         elementToEliminate.parentNode.removeChild(elementToEliminate);
-//     }
-// };
-
-function floatSemantics(elements, semantic){
-    for (var i=0; i<elements.length; i++){
-        if(isSemantic(elements[i], semantic)){
-            elements[i].parentNode.parentNode.appendChild(elements[i]);
-            //nuevo-padre.appendChild(elemento-en-cuestion)
-            debugger;
-         
+//Remueve los elementos no semanticos del html
+function removeNotSemantic() {
+   for(var i=elements.length-1;i>=0;i--){
+        if(!isSemantic(elements[i])){
+            elements[i].parentNode.removeChild(elements[i]);
         }
-        else{
-            floatSemantics(elements[i], semantic);
-            
-        };
-    };
-};
-
-//document.getElementById("someOtherDiv").appendChild(photo);
-
-// element.parentNode.removeChild(element)
-
-
-// documentHtml = JSON.parse(JSON.stringify(document));
-// var arrayElements = []
-
-// for(var i=0; i<semantic.length; i++){
-//     var elements= document.getElementsByTagName(semantic[i]);
-//     for(var j=0;j<elements.length;j++){
-//         arrayElements.push(elements[j]);
-//         debugger
-//     }
-// }
-
-
-// arrayElements[4].removeAttribute("class")
-// arrayElements[4].removeAttribute("id")
-
-
-
-var htmlString = document.getElementsByTagName("html")[0].innerHTML;
-// Faltaba añadir \ para que detectara todo el regex
-var regex = /<(([a-z])+[0-9]*(\ |>))|(<\/[a-z]+>)/g;
-var regexTags = htmlString.match(regex);
-var tagList = [
-  "<head>",
-  "</head>",
-  "<article>",
-  "</article>",
-  "<audio>",
-  "</audio>",
-  "<button>",
-  "</button>",
-  "<form>",
-  "</form>",
-  "<video>",
-  "</video>",
-  "<aside>",
-  "</aside>",
-  "<details>",
-  "</details>",
-  "<figcaption>",
-  "</figcaption>",
-  "<figure>",
-  "</figure>",
-  "<footer>",
-  "</footer>",
-  "<header>",
-  "</header>",
-  "<img>",
-  "<main>",
-  "</main>",
-  "<mark>",
-  "</mark>",
-  "<nav>",
-  "</nav>",
-  "<section>",
-  "</section>",
-  "<summary>",
-  "</summary>",
-  "<time>",
-  "</time>",
-  "</summary>",
-];
-
-for (var i = 0; i < regexTags.length; i++) {
-  if (regexTags[i].indexOf(" ") !== -1) {
-    regexTags[i] = regexTags[i].slice(0, -1);
-    regexTags[i] += ">";
-  }
+    }
 }
 
-regexTags.filter(function (tag) {
-  if (tagList.includes(tag)) {
-    return tag;
-  }
-});
-
-var semanticTags = [];
-for (var j = 0; j < regexTags.length; j++) {
-  if (tagList.includes(regexTags[j])) {
-    semanticTags.push(regexTags[j]);
-  }
+//limpia los campos sematicos
+function clearSemantic() {
+    for(var i=elements.length-1;i>=0;i--){
+        if(isSemantic(elements[i])){
+            for(var j=0;j<atribute.length;j++){
+                elements[i].removeAttribute(atribute[j]);
+            }
+        }
+    }
+    return true;
 }
 
-htmlpage = semanticTags.join("");
+//Funcion que hay que llamar desde la consola
+function transformWeb() {
+    //Primero saca a flote todas las funciones semanticas
+    classify();
+    //Una vez están a flote es seguro eliminar las no semanticas, así que las borra
+    removeNotSemantic();
+    //Ahora limpia toda la informacion sobrante
+    clearSemantic();
+}
