16bc2a5a415cbf1cd6790b0f878dee455f38f354
Khaoula Tanane <53517873+Tanane@users.noreply.github.com>
2020-04-14 16:11:00 +0200

    Revert "add array methods simulations"

--- staff/khaoula-tanane/arrays/array-functions/every.js
+++
@@ -1,8 +0,0 @@
-function every(array, callback){
-    for(var i = 0; i < array.length; i++){
-        if(!callback(array[i], i, array)){
-            return false
-        }
-    }
-    return true
-}
--- staff/khaoula-tanane/arrays/array-functions/find.js
+++
@@ -1,6 +0,0 @@
-function find(array, callback){
-    for(var i = 0; i < array.length; i++){
-        var result = callback(array[i], i, array)
-        if (result) return array[i]
-    }
-}
