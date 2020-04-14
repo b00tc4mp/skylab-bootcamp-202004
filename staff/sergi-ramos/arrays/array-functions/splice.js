1830194f94aaee46a0358651f5c4b8af4c536ef8
mac <mac@MacBook-Pro-de-mac.local>
2020-04-14 17:24:11 +0200

    apply changes

--- staff/sergi-ramos/arrays/array-functions/splice.js
+++ staff/sergi-ramos/arrays/array-functions/splice.js
@@ -1,20 +1,25 @@
 
 
 
-function splice(array, start, deleteCount, ...args) {// 1,2
+function splice(array, start, deleteCount, ...args) { // 1,2
     debugger
-    var array = [1,2,3,4,5];
+    
     var arrChange = []
     var cont = 0;
+
     for (var i = start; i < start + deleteCount; i++) {
        
         arrChange[cont] = array[i];
+        
         cont++
+
     }
-  
+    
+    
+ 
     return arrChange
 
 }
 
-
+var array = [1,2,3,4,5];
 var arr = splice(array,1,2);
