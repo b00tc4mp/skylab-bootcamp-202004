b1fe7fb9046b9129b225ecd16dacc10054d48a05
luislourencos <luis.lourencos@gmail.com>
2020-04-15 22:55:46 +0200

    add Arroz includes

--- staff/sergio-luis/arrays/arroz/arroz.prototype.includes.js
+++ staff/sergio-luis/arrays/arroz/arroz.prototype.includes.js
@@ -2,7 +2,7 @@
 
 
 Arroz.prototype.includes = function (searchElement, fromIndex = 0) {
-    
+    if(arguments.length === 0) return undefined
     for (var i = fromIndex; i < this.length; i++)
     if(this[i] === searchElement ) return true
          
--- staff/sergio-luis/arrays/arroz/arroz.prototype.includes.spec.js
+++ staff/sergio-luis/arrays/arroz/arroz.prototype.includes.spec.js
@@ -1,22 +1,29 @@
 "use estrict"
 
-describe('Arroz.prototype.includes', function () {
-    it('includes return a bollean if find return true', function () {
-        var array = new Arroz(5,2,1);
+describe('Arroz.prototype.includes', function() {
+    it('includes return a bollean if find return true', function() {
+        var array = new Arroz(5, 2, 1);
 
-        var result1 = includes(array,2);
-        var result2 = includes(array,6);
+        var result1 = array.includes(2);
+        var result2 = array.includes(6);
 
         expect(result1).toBe(true);
         expect(result2).toBe(false);
     });
-    it('includes test with string an with fromIndex return a bollean', function () {
-        var array = new Arroz('Hola','que','Tal');
+    it('includes test with string an with fromIndex return a bollean', function() {
+        var array = new Arroz('Hola', 'que', 'Tal');
 
-        var result1 = includes(array,'que',3);
-        var result2 = includes(array,'Hola',-1);
+        var result1 = array.includes('que', 3);
+        var result2 = array.includes('Hola', -1);
 
         expect(result1).toBe(false);
         expect(result2).toBe(true);
+    });
+    it('If you don`t pass any arguments to includes sould return undefined', function() {
+        var array = new Arroz('Hola', 'que', 'Tal');
+
+        var result = array.includes();
+
+        expect(result).toBe(undefined);
     });
 });
