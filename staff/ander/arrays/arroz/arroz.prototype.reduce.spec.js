d82bf5f9481499314b8261b74351febe20a1b99c
Ander Martin Ripa <andermartinr@gmail.com>
2020-04-17 00:26:27 +0200

    ....

--- staff/ander/arrays/arroz/arroz.prototype.reduce.spec.js
+++ staff/ander/arrays/arroz/arroz.prototype.reduce.spec.js
@@ -1,18 +1,60 @@
-describe("reduce", function () {
-    it("Should itereate on the array and return the value of the match", function () {
-      var array = new Arroz(1, 2, 3, 4);
-      var array1=new Arroz(1,3,1);
-      match=array.reduce (function (acumulator,value) {
-        return acumulator+value;
-      },3);
-      match1=array1.reduce(function (acumulator,value) {
-        return acumulator-value;
-      });
-      // 1 + 2 + 3 + 4
-      // expected output: 10
-        expect(match).toBe(13);
-        expect(match1).toBe(-3);
+"use strict"
+
+describe('Arroz.prototype.reduce', function() {
+    it('The reduce() method executes a reducer function (that you provide) on each element of the array, resulting in a single output value.', function() {
+        var array = new Arroz(1, 2, 3, 4);
+
+        var result1 = array.reduce(function(acc, cur) {
+            return acc + cur
+        }, );
+        var result2 = array.reduce(function(acc, cur) {
+            return acc + cur
+        }, 10);
+
+
+        expect(result1).toBe(10);
+        expect(result2).toBe(20);
     });
- 
-  });
- 
+    it('The reduce() Errors', function() {
+        var array = new Arroz();
+        var resultError = undefined;
+
+        try {
+            array.reduce(function(acc, cur) {
+                return acc + cur
+            }, );
+        } catch (error) {
+            resultError = error
+        }
+
+        expect(resultError).toBeDefined();
+        expect(resultError instanceof TypeError).toBeTruthy();
+        expect(resultError.message).toBe('Reduce of empty array with no initial value at Arroz.reduce');
+
+        var array = new Arroz(1,2,3,4);
+        var resultError = undefined;
+
+        try {
+            array.reduce();
+        } catch (error) {
+            resultError = error
+        }
+
+        expect(resultError).toBeDefined();
+        expect(resultError instanceof TypeError).toBeTruthy();
+        expect(resultError.message).toBe('undefined is not a function');
+
+        var array = new Arroz(1,2,3,4);
+        var resultError = undefined;
+
+        try {
+            array.reduce('hola mundo');
+        } catch (error) {
+            resultError = error
+        }
+
+        expect(resultError).toBeDefined();
+        expect(resultError instanceof TypeError).toBeTruthy();
+        expect(resultError.message).toBe('hola mundo is not a function');
+    });
+});
