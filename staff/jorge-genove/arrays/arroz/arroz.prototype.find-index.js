b65cfac4387dc7139ba65124a4c2a0a901f13d39
LLorch1984 <jorgegenove@gmail.com>
2020-04-16 15:17:56 +0200

    find-index bug fixed

--- staff/jorge-genove/arrays/arroz/arroz.prototype.find-index.js
+++ staff/jorge-genove/arrays/arroz/arroz.prototype.find-index.js
@@ -1,11 +1,11 @@
 Arroz.prototype.findIndex = function(callback){
-    if( typeof callback !== 'function'){
+     if(typeof callback !== 'function') throw new TypeError(callback + ' is not a function') 
         for (var i = 0; i < this.length; i++){
             if(callback(this[i])){
                 return i
             }
         }
    
-    }
+    
 return -1
 }
