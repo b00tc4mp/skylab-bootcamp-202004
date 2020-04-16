f40d71965a7c6cfa444d5ce1769d9d770ca8e100
tanane <tananekhaoula@gmail.com>
2020-04-16 10:04:12 +0200

    add declaration inside the function

--- staff/khaoula-tanane/arrays/arroz/arroz.prototype.index-of.js
+++ staff/khaoula-tanane/arrays/arroz/arroz.prototype.index-of.js
@@ -1,4 +1,5 @@
-Arroz.prototype.indexOf = function(string, start=0){
+Arroz.prototype.indexOf = function(string, start){
+    var start = 0
     for(var i = start; i < this.length; i++){
         if(this[i] === string){
             return i;
