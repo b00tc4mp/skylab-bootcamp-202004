aebacb3a2ca1a292a0f98c833563ac69644227b1
LLorch1984 <jorgegenove@gmail.com>
2020-04-17 10:18:25 +0200

    slice function

---
+++ staff/jorge-genove/arrays/arroz/arroz.prototype.slice.js
@@ -0,0 +1,21 @@
+Arroz.prototype.slice = function (begin, end) {
+  slicedValues = [];
+  if (begin > this.length) {
+    return sliceValues;
+  }
+  if (typeof begin === "undefined") {
+    begin = 0;
+  }
+  if (begin < 0) {
+    begin === slicedValues.length + begin;}
+    if (end < 0) {
+      end === sliceValues.length + end;
+    }
+    if (end === 'undefined' || end > this.length){
+        end = this.length
+    }
+  
+  for (var i = begin; i < end - 1; i++) {
+    slicedValues[slicedValues.length] = this[i];
+  }
+};
