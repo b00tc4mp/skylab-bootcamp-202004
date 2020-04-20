59615db5ef7594617732faef68d3811d001e79ea
jordigraup <jordigptab@gmail.com>
2020-04-20 09:09:40 +0200

    code own methods and prototypes

--- staff/jordi-grau/arrays/array-functions/map.js
+++ staff/jordi-grau/arrays/array-functions/map.js
@@ -1,9 +1,10 @@
-'use strict'
-
 function map(array,expression){
     var result = [];
     for (var i = 0; i <array.length; i++){
-        result[i] = expression(result[i])
+        result[i] = expression(array[i],i,array)
     }
     return result
 }
+
+
+
