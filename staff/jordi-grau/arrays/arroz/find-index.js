f7bdd54a7544bf42438b524597c362b36593758f
jordigraup <jordigptab@gmail.com>
2020-04-20 12:21:07 +0200

    add methods files

---
+++ staff/jordi-grau/arrays/arroz/find-index.js
@@ -0,0 +1,11 @@
+'use strict'
+
+function findIndex(array,expression){
+
+    for(var i=0; i< array.length; i++){
+        if(expression(array[i])){
+            return i
+        }
+    }
+    return -1
+}
