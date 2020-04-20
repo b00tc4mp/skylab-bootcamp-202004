6cf9911d9ab4f218b2f7113bc38fea2f2d0740a9
Sergi Borja Gutiérrez <sergiborja05@gmail.com>
2020-04-21 01:55:31 +0200

    register feedback

---
+++ staff/sergi-borja/web-components/my-app/components/Register.js
@@ -0,0 +1,59 @@
+function Register(onSubmit, onLogin) {
+    const temp = document.createElement('div')
+
+    temp.innerHTML = `<section class="register">
+    <h1>Register</h1>
+    <form>
+        <input type="text" name="name" placeholder="name">
+        <input type="text" name="surname" placeholder="surname">
+        <input type="email" name="email" placeholder="e-mail">
+        <input type="password" name="password" placeholder="password">
+        <button>Submit</button>
+        or <a href="">Login</a>
+    </form>
+</section>`
+
+    const container = temp.firstChild
+
+    const form = container.querySelector('form')
+
+    let feedback
+
+    form.addEventListener('submit', function (event) {
+        event.preventDefault()
+
+        const name = event.target.name.value,
+            surname = event.target.surname.value,
+            email = event.target.email.value,
+            password = event.target.password.value
+
+    try{
+        onSubmit(name, surname, email, password)
+        
+        event.target.name.value = '';
+        event.target.surname.value = '';
+        event.target.email.value = '';
+        event.target.password.value = '';
+
+    }catch(error) {
+        if(!feedback) {
+            feedback= Feedback(error.message, 'error')
+
+            container.append(feedback)
+        }else{
+            feedback.innerText = error.message
+        }
+    }
+            
+    })
+
+    const login = container.querySelector('a')
+
+    login.addEventListener('click', function(event) {
+        event.preventDefault()
+
+        onLogin()
+    })
+
+    return container
+}
---
+++ staff/sergi-borja/web-components/my-app/main.js
@@ -0,0 +1,62 @@
+const users = [
+    {
+      name: "Manuel",
+      surname: "Barzi",
+      email: "manuelbarzi@gmail.com",
+      password: "123",
+    },
+  ];
+  
+  const landing = Landing(
+    function () {
+      landing.replaceWith(register);
+    },
+    function () {
+      landing.replaceWith(login);
+    }
+  );
+  
+  const register = Register(
+    function (name, surname, email, password) {
+      const repeatMail = users.find(function (user) {
+        return user.email === email;
+      });
+  
+      if (repeatMail) {
+        throw new Error("Email already exists MDRFUCKER");
+      } else {
+        users.push({
+          name,
+          surname,
+          email,
+          password,
+        });
+        register.replaceWith(login);
+      }
+    },
+    function () {
+      register.replaceWith(login);
+    }
+  );
+  
+  const login = Login(
+    function (email, password) {
+      const user = users.find(function (user) {
+        return user.email === email && user.password === password;
+      });
+  
+      if (user) {
+        const home = Home(user.name, function () {
+          home.replaceWith(landing);
+        });
+  
+        login.replaceWith(home);
+      } else throw new Error("wrong credentials");
+    },
+    function () {
+      login.replaceWith(register);
+    }
+  );
+  
+  document.getElementById("root").appendChild(landing);
+  
