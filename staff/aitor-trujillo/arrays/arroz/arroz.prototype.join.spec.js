700a2b1b4bc537c06724016d09216b012a5e0346
Aitor1111 <lonesniper11@gmail.com>
2020-04-20 09:12:42 +0200

    add features requested in comments & refactorize

--- staff/aitor-trujillo/arrays/arroz/arroz.prototype.join.spec.js
+++ staff/aitor-trujillo/arrays/arroz/arroz.prototype.join.spec.js
@@ -9,6 +9,13 @@ describe("Arroz.prototype.join", function () {
     expect(typeof result).toBe("string");
     expect(result.length).toBe(15);
   });
+  it("should use the default separator by commas", function () {
+    var myArroz = new Arroz("hello", 1, 2, "world");
+
+    var result = myArroz.join();
+
+    expect(result[5]).toBe(",");
+  });
   it("should join all items of array into string and return it with separator comma (not the last one)", function () {
     var myArroz = new Arroz("hello", 1, 2, "world");
 
--- staff/aitor-trujillo/arrays/arroz/arroz.prototype.pop.spec.js
+++ staff/aitor-trujillo/arrays/arroz/arroz.prototype.pop.spec.js
@@ -9,4 +9,18 @@ describe("Arroz.prototype.pop", function () {
     expect(arroz[1]).toBe(2);
     expect(arroz[2]).toBe(undefined);
   });
+  it("should return the deleted item", function () {
+    var arroz = new Arroz(1, 2, 3);
+
+    var deleted = arroz.pop();
+
+    expect(deleted).toBe(3);
+  });
+  it("should change the length of the array", function () {
+    var arroz = new Arroz(1, 2, 3);
+
+    arroz.pop();
+
+    expect(arroz.length).toBe(2);
+  });
 });
