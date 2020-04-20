80cd38ab6dfc63d07d473773c58df0e7f90ab709
luislourencos <luis.lourencos@gmail.com>
2020-04-20 13:55:00 +0200

    add folder web-components

---
+++ staff/sergio-luis/web-components/my-app/main.js
@@ -0,0 +1,33 @@
+const users = []
+
+const register = Register(function (name, surname, email, password) {
+    users.push({
+        name,
+        surname,
+        email,
+        password
+    })
+
+    register.replaceWith(login)
+})
+
+const login = Login(function (email, password) {
+
+    const user = users.find(function(user) { 
+        return user.email === email && user.password === password 
+    })
+    const home = Home(user.name)
+    if (user) {
+        login.replaceWith(home)
+    }else {
+        console.error('wrong credentials');
+    }
+
+})
+
+var home = Home();
+
+
+
+
+document.getElementById('root').appendChild(register)
