56b83b4703e082960cad3ec1c15347cb1085efc1
betho <vetu87@gmail.com>
2020-04-20 14:04:49 +0200

    feature/web-components first contact

---
+++ staff/albert-manzano/web-components/my-app/components/Home.js
@@ -0,0 +1,16 @@
+// TODO show "Welcome, <name>!"
+function Home(name) {
+    const temp = document.createElement('div')
+
+    temp.innerHTML = `<section class="home">
+    <h1>Welcome ${name}</h1>
+    
+</section>`
+
+
+    const container = temp.firstChild
+
+    
+
+    return container
+}
