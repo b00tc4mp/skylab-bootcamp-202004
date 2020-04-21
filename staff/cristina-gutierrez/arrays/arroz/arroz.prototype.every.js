670a40ee8f86913c564f4719e2365d914259bafa
crispine <gutierrezales@hotmail.com>
2020-04-21 18:53:07 +0200

    arroz every arranged

--- staff/cristina-gutierrez/arrays/arroz/arroz.prototype.every.js
+++ staff/cristina-gutierrez/arrays/arroz/arroz.prototype.every.js
@@ -2,9 +2,9 @@ Arroz.prototype.every = function (callback) {
     if (typeof callback !== 'function') throw new TypeError(callback + ' is not a function');
     
     for(var i = 0; i < this.length; i++) {
-        if (callback(this[i], i, this)) {
-            return true
-        };
+        if (!(callback(this[i], i, this))) {
+            return false;
+        }
     };
-    return false;
+    return true;
 };
