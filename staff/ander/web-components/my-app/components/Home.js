e74c51e882cb3308aabdc77f58e64a569c282734
Ander Martin Ripa <andermartinr@gmail.com>
2020-04-21 20:15:08 +0200

    added

---
+++ staff/ander/web-components/my-app/components/Home.js
@@ -0,0 +1,36 @@
+function Home(name, onLogout){
+    const temp= document.createElement("div");
+
+    temp.innerHTML = ` <section> 
+    <span> Welcome ${name} </span>
+    <input id="search" type="text" name="search" placeholder="search">
+    <button id="logout">Logout</button>
+    <div id="listusers"></div>
+    </section>`
+
+    const container = temp;
+
+    const input = container.querySelector("#search")
+
+    const listUsers = container.querySelector("#listusers")
+
+    input.addEventListener("keyup", (event)=>{
+        console.log(event.target.value)
+        const _users = searchUsers(event.target.value)
+        console.log(_users)
+
+        listUsers.innerHTML = ''
+
+        _users.forEach(user=>{
+            listUsers.innerHTML += `<p> ${user.name} </p>`
+        })
+
+    })
+
+    const logout = container.querySelector("#logout")
+    logout.addEventListener("click",function(){
+        onLogout()
+    })
+
+    return container;
+}
---
+++ staff/ander/web-components/my-app/logic/search-users.js
@@ -0,0 +1,39 @@
+// TODO show "Welcome, <name>!"
+function Home(name, onLogout){
+    const temp= document.createElement("div");
+
+    temp.innerHTML = ` <section> 
+    <span> Welcome ${name} </span>
+    <input id="search" type="text" name="search" placeholder="search">
+    <button id="logout">Logout</button>
+    <ul id="listusers"></ul>
+    </section>`
+
+    
+
+    const container = temp;
+
+    const input = container.querySelector("#search")
+
+    const listUsers = container.querySelector("#listusers")
+
+    input.addEventListener("keyup", (event)=>{
+        const _users = searchUsers(event.target.value)
+        console.log(_users)
+        listUsers.innerHTML = ''
+        
+        _users.forEach(user=>{
+            var listElements = document.createElement("li");
+            listElements.appendChild(document.createTextNode(user.name + ' ' + user.surname));
+            listUsers.appendChild(listElements);
+            //listUsers.innerHTML += `<p> ${user.name} </p>`
+        })
+        
+    })
+    const logout = container.querySelector("#logout")
+    logout.addEventListener("click",function(){
+        onLogout()
+    })
+
+    return container;
+}
