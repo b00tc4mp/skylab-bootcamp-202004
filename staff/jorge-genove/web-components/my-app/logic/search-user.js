27e698b6862d7d3b7679e13d88b15bb29b0f007e
LLorch1984 <jorgegenove@gmail.com>
2020-04-22 14:10:47 +0200

    testing retrive

--- staff/jorge-genove/web-components/my-app/logic/search-user.js
+++ staff/jorge-genove/web-components/my-app/logic/search-user.js
@@ -14,16 +14,3 @@ return user
 
 
 
-// function searchUsers(query) {
-    // TODO find users matching query in name, surname, email
-
-    // const _users = users.filter(function(user) {
-        // TODO match user.name contains query || user.surname contains query || ...
-    // })
-
-    // TODO sanitize: create new objects of users without password
-
-    // TODO return _users
-// 
-
-
