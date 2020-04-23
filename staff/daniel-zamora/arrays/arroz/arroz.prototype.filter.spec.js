a2885de0870c9d5bc022eb4f5ef1e84bc274e8a7
60225198 <daniel_zamora27@me.com>
2020-04-23 19:43:20 +0200

    unhappy paths add to filter method

--- staff/daniel-zamora/arrays/arroz/arroz.prototype.filter.spec.js
+++ staff/daniel-zamora/arrays/arroz/arroz.prototype.filter.spec.js
@@ -42,4 +42,26 @@ describe("Arroz.prototype.filter", function () {
  
   });
 
+  it("should fail if any parameter its applied", function () {
+    var array = new Arroz(1, 2, 3, 4, 5, 6, 7, 8);
+
+    try {
+      var matchArray = array.filter() > 5;
+    } catch (error) {
+      matchArray = error;
+    }
+    expect(matchArray instanceof Error).toBeTruthy();
+  });
+
+  it("should fail if the callbakc is not a function", function () {
+    var array = new Arroz(1, 2, 3, 4, 5, 6, 7, 8);
+
+    try {
+      var matchArray = array.filter(array) > 5;
+    } catch (error) {
+      matchArray = error;
+    }
+    expect(matchArray instanceof Error).toBeTruthy();
+  });
+
 });
