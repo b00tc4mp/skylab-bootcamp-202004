5df5a346f18c33485b727318397f2ef3f99c7cbe
tanane <tananekhaoula@gmail.com>
2020-04-23 12:51:31 +0200

    add fixes

--- staff/khaoula-tanane/web-components/my-app/components/Result.js
+++ staff/khaoula-tanane/web-components/my-app/components/Result.js
@@ -9,11 +9,8 @@ function Results(_users) {
     if (_users.length) {
         const list = document.createElement('ul')
 
-        // users.forEach(function (user) {
         _users.forEach(function ({ name, surname, email }) {
             const item = document.createElement('li')
-
-            // const { name, surname, email } = user
 
             item.innerText = `${name} ${surname} ${email}`
 
