6c3a881ecafa39e44386c1a761497f1953a3142a
alejandromc23 <alejandro.mascort17@hotmail.com>
2020-04-26 12:36:40 +0200

    add new logic testing cases

--- staff/alejandro-mascort/web-components/my-app/logic/google.spec.js
+++ staff/alejandro-mascort/web-components/my-app/logic/google.spec.js
@@ -1,5 +1,5 @@
 describe('google', () => {
-    NodeIterator('should succeed on matching query', done => {
+    it('should succeed on matching query', done => {
         google('hello-world', function(error, results) {
             expect(error).to.be.undefined
 
@@ -12,6 +12,19 @@ describe('google', () => {
                 expect(link).to.be.a('string')
             })
 
+            done()
+        })
+    })
+
+    it('should succed and return an empty array if non existing content is searched', done => {
+        google('asdasdwergfw5454654453r3er356456321223rfsds', (error, results) => {
+            expect(error).to.be.undefined
+
+            expect(results).to.exist
+            expect(results).to.be.an('array')
+            expect(results.length).to.equal(0)
+
+            done()
         })
     })
 })
