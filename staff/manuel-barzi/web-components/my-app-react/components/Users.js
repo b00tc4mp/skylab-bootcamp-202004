cb1ebb7bb750f00056f55997f369da2b64cd0f7c
manuelbarzi <manuelbarzi@gmail.com>
2020-04-27 17:35:23 +0200

    boost my-app-react

---
+++ staff/manuel-barzi/web-components/my-app-react/components/Users.js
@@ -0,0 +1,25 @@
+// class Users extends Component {
+//     constructor(message, level) {
+//         super(`<section class="users">
+//             <h2>Users</h2>
+//         </section>`)
+
+//         let results
+
+//         this.container.append(new Search(query => {
+//             const users = searchUsers(query)
+
+//             if (!results) {
+//                 results = new Results(users)
+
+//                 this.container.append(results.container)
+//             } else {
+//                 const _results = results
+
+//                 results = new Results(users)
+
+//                 _results.container.replaceWith(results.container)
+//             }
+//         }).container)
+//     }
+// }
