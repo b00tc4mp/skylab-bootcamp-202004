f3d13227c5f35b8f315e527c6b14d0ed650b90f2
alejandromc23 <alejandro.mascort17@hotmail.com>
2020-04-27 13:28:20 +0200

    add flow improvements

--- staff/alejandro-mascort/web-components/my-app/components/Home.js
+++ staff/alejandro-mascort/web-components/my-app/components/Home.js
@@ -1,47 +1,70 @@
 class Home extends Component {
     constructor(name, toLanding) {
         super(`<section class="home">
-        <h1>Welcome, ${name}!</h1><button>Logout</button>
-        <a href="">Hide/Display Latest News</a>
+        <h1>Welcome, ${name}!</h1>
+        <a href="" class="anchors">Latest News</a>
+        <a href="" class="anchors">Google Something</a>
+        <a href="" class="anchors">Search Users</a>
+        <button>Logout</button>
     </section>`)
     
         const button = this.container.querySelector('button')
         
-        const anchor = this.container.querySelector('a')
+        const anchors = this.container.querySelectorAll('a')
+
+        let result
+
+        const news = anchors[0]
+        const navigator = anchors[1]
+        const searchUsers = anchors[2]
+
+        const lastnews = new News()
+        const googleNavigator = new Navigate()
+        const users = new Search(query => {
+            if (query.trim().length > 0 && typeof query != 'undefined') {
+                if (result) this.container.removeChild(result.container)
+                result = new Results(query)
+                
+                users.container.appendChild(result.container)
+            }
+        })
+        
+        let current = lastnews.container
+        news.style.backgroundColor = '#FA8072'
 
         button.addEventListener('click', () => {
             toLanding()
         })
 
-        let result
-
-        this.container.append(new Search(query => {
-            if (query.trim().length > 0 && typeof query != 'undefined') {
-                if (result) this.container.removeChild(result.container)
-                result = new Results(query)
-                
-                this.container.append(result.container)
-            }
-        }).container)
-
-        this.container.append(new Navigate().container)
-
-        let latestNewsDisplayed = true
-        const lastnews = new News()
-
         this.container.append(lastnews.container)
 
-        anchor.addEventListener('click', event => {
-            event.preventDefault()
-            debugger
-            if (latestNewsDisplayed) {
-                this.container.removeChild(lastnews.container)
-                latestNewsDisplayed = false
-            } else {
-                this.container.append(lastnews.container)
-                latestNewsDisplayed = true
-            }
+        news.addEventListener('click', event => {
+            replaceSection(lastnews)
+
+            news.style.backgroundColor = '#FA8072'
         })
+
+        navigator.addEventListener('click', event => {
+            replaceSection(googleNavigator)
+
+            navigator.style.backgroundColor = '#FA8072'
+        })
+
+        searchUsers.addEventListener('click', event => {
+            replaceSection(users)
+
+            searchUsers.style.backgroundColor = '#FA8072'
+        })
+
+        const replaceSection = section => {
+            event.preventDefault()
+
+            anchors.forEach(anchor => anchor.style.backgroundColor = '#FFA07A')
+
+            current.replaceWith(section.container)
+
+            current = section.container
+        }
     }   
 }
 
