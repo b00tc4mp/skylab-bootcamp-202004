f6ce210e80ede0df15d469b30930e0dd10f94765
betho <vetu87@gmail.com>
2020-04-27 16:28:12 +0200

    red dots removed

--- staff/albert-manzano/web-components/app-custom/logic/amazon.js
+++ staff/albert-manzano/web-components/app-custom/logic/amazon.js
@@ -47,24 +47,29 @@ function amazon(query, callback) {
 
     var xhr = new XMLHttpRequest();
 
-    xhr.open('GET', `https://skylabcoders.herokuapp.com/proxy?url=https://www.amazon.es/s?k=${query}`)​
-        xhr.onload = function () {
+    xhr.open('GET', `https://skylabcoders.herokuapp.com/proxy?url=https://www.amazon.com/s?k=${query}`)​
+    xhr.onload = function () {
         const parser = new DOMParser()​
         const doc = parser.parseFromString(this.responseText, 'text/html')​
         const results = doc.querySelectorAll('.a-spacing-medium')​
         var items = [];
-            results.forEach(result => {
-                let price = result.querySelector('.a-price-whole')
-                let image = result.querySelector('.s-image')
-                let content = result.querySelector('.a-text-normal')
-                let link = result.querySelector('.a-link-normal')
-                if(price !== null && image !== null && content !== null && link !== null){
-                    image=image.attributes[0]
-                    price=price.innerText
-                    content=content.innerText
-                    link=link.href
-                    items.push({link,content,image,price});
-                }
+        results.forEach(result => {
+            let price = result.querySelector('.a-price-whole')
+            let image = result.querySelector('.s-image')
+            let content = result.querySelector('.a-text-normal')
+            let link = result.querySelector('.a-link-normal')
+            if (price !== null && image !== null && content !== null && link !== null) {
+                image = image.attributes[0]
+                price = price.innerText
+                content = content.innerText
+                link = link.href
+                items.push({
+                    link,
+                    content,
+                    image,
+                    price
+                });
+            }
         });
 
         callback(undefined, items);
