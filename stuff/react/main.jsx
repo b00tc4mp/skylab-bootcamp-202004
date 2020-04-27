c2d6a5e03d31df1d3bd638e48f1801438cbddfd0
manuelbarzi <manuelbarzi@gmail.com>
2020-04-27 16:23:12 +0200

    boost react

--- stuff/react/main.jsx
+++ stuff/react/main.jsx
@@ -1,10 +1,4 @@
-const helloWorld = <h1>Hola, Mundo!</h1>
-
 const names = ['Mary', 'Anna', 'John', 'Max', 'James']
-
-const items = names.map(name => <li>{name}</li>)
-
-const namesList = <ul>{items}</ul>
 
 const colors = ['Red', 'Green', 'Blue', 'Yellow', 'Orange']
 
@@ -18,6 +12,95 @@ function List(props) {
     return list
 }
 
-const colorList = List({ items: colors })
+function PeopleAndColors(props) {
+    const { colors, people } = props
 
-ReactDOM.render([helloWorld, namesList, colorList], document.getElementById('root'))
+    // return <section>
+    //     <List items={people} />
+    //     <List items={colors} />
+    // </section>
+
+    return <>
+        <List items={people} />
+        <List items={colors} />
+    </>
+}
+
+const { Component } = React
+
+function Header({ view, onChangeView }) {
+    return <header>
+        <nav>
+            <ul>
+                <li>
+                    <a className={`header-link ${view === 'home' ? 'header-link--active' : ''}`} href="" onClick={event => {
+                        event.preventDefault()
+
+                        onChangeView('home')
+                    }}>Home</a>
+                </li>
+
+                <li>
+                    <a className={`header-link ${view === 'about' ? 'header-link--active' : ''}`} href="" onClick={event => {
+                        event.preventDefault()
+
+                        onChangeView('about')
+                    }}>About</a>
+                </li>
+
+                <li>
+                    <a className={`header-link ${view === 'contact' ? 'header-link--active' : ''}`} href="" onClick={event => {
+                        event.preventDefault()
+
+                        onChangeView('contact')
+                    }}>Contact</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+}
+
+function Home() {
+    return <>
+        <h1>Home</h1>
+
+        <PeopleAndColors colors={colors} people={names} />
+    </>
+}
+
+function About() {
+    return <h1>About</h1>
+}
+
+function Contact() {
+    return <h1>Contact</h1>
+}
+
+class App extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            view: 'home'
+        }
+
+        //this.handleChangeView = this.handleChangeView.bind(this)
+    }
+
+    // handleChangeView(view) {
+    //     this.setState({ view })
+    // }
+
+    handleChangeView = view => this.setState({ view })
+
+    render() {
+        return <>
+            <Header view={this.state.view} onChangeView={this.handleChangeView} />
+
+            {this.state.view === 'home' && <Home />}
+            {this.state.view === 'about' && <About />}
+            {this.state.view === 'contact' && <Contact />}
+        </>
+    }
+}
+ReactDOM.render(<App />, document.getElementById('root'))
