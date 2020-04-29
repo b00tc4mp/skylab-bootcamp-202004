3ae7112f5a3602027c2cd39c24006836e7839fa4
alejandromc23 <alejandro.mascort17@hotmail.com>
2020-04-29 09:49:07 +0200

    total conversion to react

--- staff/alejandro-mascort/web-components/my-app-react/components/GoogleResults.jsx
+++ staff/alejandro-mascort/web-components/my-app-react/components/GoogleResults.jsx
@@ -1,34 +1,3 @@
-// class GoogleResults extends Component {
-//     constructor(results) {
-//         super(`<section class="results">
-//     </section>`)
-
-//         if (results.length) {
-//             const list = document.createElement('ul')
-
-//             results.forEach(({ title, content, link }) => {
-//                 const item = document.createElement('li')
-
-//                 const _link = document.createElement('a');
-//                 _link.href = link;
-//                 _link.target = '_blank';
-//                 _link.innerText = title;
-
-//                 item.append(_link);
-
-//                 const _content = document.createElement('p')
-//                 _content.innerText = content
-
-//                 item.append(_content)
-
-//                 list.append(item)
-//             })
-
-//             this.container.append(list)
-//         } else this.container.append(new Feedback('sorry, no results :(', 'warning').container)
-//     }
-// }
-
 function GoogleResults({results}) {
     let list = results.map(({title, content, link}) => <li><a href={link} target='_blank'>{title}</a> <p>{content}</p></li>)
     
--- staff/alejandro-mascort/web-components/my-app-react/components/HolaNews.jsx
+++ staff/alejandro-mascort/web-components/my-app-react/components/HolaNews.jsx
@@ -1,64 +1,29 @@
-import { Component } from "react";
+class HolaNews extends Component{
+    constructor() {
+        super()
 
-// class HolaNews extends Component {
-//     constructor(message, level) {
-//         super(`<section class="hola-news">
-//             <h2>Hola News</h2>
-//         </section>`)
+        this.state = {
+            error: undefined,
+            results: undefined
+        }
+    }
 
-//         retrieveHolaNews((error, news) => {
-//             if (error) throw error // TODO handle this error!
+    handleSubmit = () => {
+        retrieveHolaNews((error, results) => {
+            if (error) this.setState({error: error.message})
+            else {
+                let list = results.map(({image, link, text}) => <li><a href={link} target='_blank'><img src={image}/></a> <p>{text}</p></li>)
+                this.setState({results: list})
+            }
+        })
+    }
 
-//             const list = document.createElement('ul')
-
-//             news.forEach(({ image, link, text }) => {
-//                 const item = document.createElement('li')
-
-//                 const anchor = document.createElement('a')
-//                 anchor.href = link
-//                 anchor.target = '_blank'
-                
-//                 item.append(anchor)
-
-//                 const img = document.createElement('img')
-//                 img.src = image
-
-//                 anchor.append(img)
-
-//                 const paragraph = document.createElement('p')
-//                 paragraph.innerText = text
-
-//                 anchor.append(paragraph)
-
-//                 list.append(item)
-//             })
-
-//             this.container.append(list)
-//         })
-//     }
-// }
-
-// class HolaNews extends Component{
-//     constructor() {
-//         super()
-
-//         this.state = {
-//             error: undefined,
-//             results: undefined
-//         }
-//     }
-
-//     handleSubmit = () => {
-//         retrieveHolaNews((error, results) => {
-//             if (error) this.setState({error: error.message})
-//             else this.setState({results: results})
-//         })
-//     }
-
-//     render() {
-//         return <section className="hola-news">
-//             <h2>Hola News</h2>
-//         </section>
-//     }
-
-// }
+    render() {
+        return <section className="hola-news">
+            <h2>Hola News</h2>
+            {this.handleSubmit()}
+            {this.state.error && <Feedback message={this.state.error} level='error' />}
+            {this.state.results && <ul>{this.state.results}</ul>}
+        </section>
+    }
+}
