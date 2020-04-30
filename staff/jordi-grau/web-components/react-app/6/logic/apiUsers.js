1f52eddee941d9dd73e22df8884843045c8f13eb
jordigraup <jordigptab@gmail.com>
2020-04-30 16:57:36 +0200

    webb commponents

--- staff/jordi-grau/web-components/react-app/6/logic/apiUsers.js
+++ staff/jordi-grau/web-components/react-app/6/logic/apiUsers.js
@@ -1,25 +1,10 @@
-function google(query, callback) {
+function apiUsers(id, callback) {
     var xhr = new XMLHttpRequest()
 
-    xhr.open( 'GET', `https://skylabcoders.herokuapp.com/proxy?url=https://www.google.com/search?q=${query}` )
+    xhr.open( 'GET', `https://skylabcoders.herokuapp.com/api/v2/users/${id}` )
 
     xhr.onload = function () {
-        const parser = new DOMParser()
-
-        const doc = parser.parseFromString(this.responseText, 'text/html')
-
-        let results = doc.querySelectorAll('.rc')
-
-        const data = []
-
-        results.forEach(result => {
-            const title = result.querySelector('.LC20lb').innerText
-
-            const content = result.querySelector('.st').innerText
-
-            const { href: link } = result.querySelector('.r > a') 
-
-            data.push({ title, content, link })
+        const user = JSON
         })
    
         callback(undefined, data)
