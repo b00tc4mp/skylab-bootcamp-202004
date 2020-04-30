99aaa2e47a10a870b2bbf5699c255845bfb5d50e
sergoustar <sergirutu@gmail.com>
2020-04-30 15:20:16 +0200

    code

--- staff/sergi-ruiz/web-components/my-app-react/components/App.jsx
+++ staff/sergi-ruiz/web-components/my-app-react/components/App.jsx
@@ -3,35 +3,31 @@ const { Component } = React
 class App extends Component {
 
     constructor() {
-        super()
-        this.state = {
-            view: 'landing',
-            user: undefined
-        }
-    }
-    handleGoToRegister = () => this.setState({ view: 'register' })
-    handleGoToLogin = () => this.setState({ view: 'login' })
-    handleLogout = () => this.setState({
-        view: 'landing',
-        user: undefined
-    })
-    handleLogin = (email, password) => {
-        loginUser(email, password)
-        const currentUser = retrieveUser(email)
-        const {name} = currentUser
-        this.setState({
-            view: 'home',
-            user: name
-        })
-    }
-    
-    render() {
 
+        super()
+
+        this.state = {
+            view: 'landing'
+        }
+
+    }
+
+    handleGoToRegister = () => this.setState({ view: 'register'})
+    handleGoToLogin = () => this.setState({view: 'login'})
+
+    handleRegister = (name, surname, email, password) => {
+        registerUser(name, surname, email, password)
+
+        this.setState({view: 'login '})
+    }
+
+    render() {
         return <>
-            {this.state.view === 'landing' && <Landing onRegister={this.handleGoToRegister} onLogin={this.handleGoToLogin} />}
-            {this.state.view === 'register' && <Register onLogin={this.handleGoToLogin} />}
-            {this.state.view === 'login' && <Login onRegister={this.handleGoToRegister} onSubmitLogin={this.handleLogin} />}
-            {this.state.view === 'home' && <Home user={this.state.user} logOut={this.handleLogout}/>}
+
+        {this.state.view === 'landing' && <Landing onRegister={this.handleGoToRegister} onLogin={this.handleGoToLogin}/>}
+        {this.state.view === 'register' && <Register onSubmit={this.handleRegister} />}
+        {this.state.view === 'home' && <Home />}
+
         </>
     }
 }
