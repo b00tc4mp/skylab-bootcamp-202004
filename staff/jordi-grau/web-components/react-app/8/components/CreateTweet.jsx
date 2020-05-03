372263143b9cfc6568d67f13de9931056c690e89
jordigraup <jordigptab@gmail.com>
2020-05-03 20:14:13 +0200

    web components twitter

---
+++ staff/jordi-grau/web-components/react-app/8/components/CreateTweet.jsx
@@ -0,0 +1,9 @@
+function createTweet (onSubmit) {
+    return <section className="createTweet"> 
+    <h2>Tweet Now</h2>
+    <form onSubmit= {onSubmmit}>
+        <input type="myTweet" name="myTweet" placeholder=" Qué está pasando? " />
+        <button>Twittear</button>
+    </form>
+    </section>
+}
