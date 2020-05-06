76d8e5328887abf4900787e5f2be3abafc6c9415
alejandromc23 <alejandro.mascort17@hotmail.com>
2020-05-06 13:45:16 +0200

    add improvements and testing

--- staff/alejandro-mascort/web-components/my-app-react/logic/is-user-authenticated.js
+++ staff/alejandro-mascort/web-components/my-app-react/logic/is-user-authenticated.js
@@ -4,12 +4,10 @@ function isUserAuthenticated(token, callback) {
     if (typeof callback !== 'function') throw new TypeError(`${callback} is not a function`)
 
     call('GET', 'https://skylabcoders.herokuapp.com/api/v2/users',
-        undefined,
-        { 'Authorization': `Bearer ${token}` },
-        (error, status) => {
-            if (error) return callback(error)
-
-            callback(undefined, status === 200)
-        }
-    )
+    undefined, {'Authorization': `Bearer ${token}`},
+    (error, status) => {
+        if (error) return callback(error)
+        if (status === 401) return callback(new Error('invalid token'))
+        callback( undefined, status === 200) 
+    })
 }
