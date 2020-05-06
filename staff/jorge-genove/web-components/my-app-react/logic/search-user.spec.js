20c6a7c6ba0e7519cb29e75385edf75391b2fac1
LLorch1984 <jorgegenove@gmail.com>
2020-05-06 19:03:08 +0200

    spinner and specs

--- staff/jorge-genove/web-components/my-app-react/logic/search-user.spec.js
+++ staff/jorge-genove/web-components/my-app-react/logic/search-user.spec.js
@@ -1,106 +1,86 @@
-describe.only("searchUser", () => {
+describe.only('searchUser', () => {
+  let name, surname, email, password,tokenValue
 
-  let tokenValue;
-  let users;
-  let email;
-  let password;
-  let name;
-  let surname;
-
-  beforeEach((done) => {
-
-    name = names.random();
-    surname = surnames.random();
-    email = `${name
-      .toLowerCase()
-      .split(" ")
-      .join("")}${surname
-      .toLowerCase()
-      .split(" ")
-      .join("")
-      .concat("-")
-      .concat(Math.random())}@mail.com`;
-    password = passwords.random();
-    //end before each here
-
-    //describe( when user exists
-    //beforeEach
-    //call for registering the user
-
-    //it()
-    //)
-
-    call(
-      "POST",
-      "https://skylabcoders.herokuapp.com/api/v2/users/",
-      `{"name": "${name}" , "surname": "${surname}", "username" : "${email}","password": "${password}"}`,
-      { "Content-type": "application/json" },
-      (error, status, body) => {
-        if (error) return done(new Error(error));
-        if (status !== 201) return done(new Error("unexpectet status"));
-
+  beforeEach(() => {
+      name = names.random()
+      surname = surnames.random()
+      email = `${name.toLowerCase().split(' ').join('')}${surname.toLowerCase().split(' ').join('').concat('-').concat(Math.random())}@mail.com`
+      password = passwords.random()
+  })
+    describe('when  user already exists', () => {
+      beforeEach(done =>{debugger
         call(
           "POST",
-          "https://skylabcoders.herokuapp.com/api/v2/users/auth",
-          `{"username" : "${email}", "password": "${password}"}`,
+          "https://skylabcoders.herokuapp.com/api/v2/users/",
+          `{"name": "${name}" , "surname": "${surname}", "username" : "${email}","password": "${password}"}`,
           { "Content-type": "application/json" },
           (error, status, body) => {
             if (error) return done(new Error(error));
-            if (status !== 200) return done(new Error("Unexpectet status"));
-            
-            const { token } = JSON.parse(body);
-            tokenValue = token;
-            
+            if (status !== 201) return done(new Error("unexpectet status"));
+    
+            call(
+              "POST",
+              "https://skylabcoders.herokuapp.com/api/v2/users/auth",
+              `{"username" : "${email}", "password": "${password}"}`,
+              { "Content-type": "application/json" },
+              (error, status, body) => {
+                if (error) return done(new Error(error));
+                if (status !== 200) return done(new Error("Unexpectet status"));
+                
+                const { token } = JSON.parse(body);
+                tokenValue = token;
+                debugger
+                done();
+              }
+            );
+          }
+        );
+      });
+      it("it should succed in name search", (done) => {debugger
+        debugger
+        searchUsers("daniel", tokenValue, () => {
+        
+            expect(users).to.exist;
+            expect(users.length).to.be.greaterThan(0);
+            done();
+          }
+        );
+      });
+    
+    
+    })
+    
+
+ /*  afterEach((done) => {
+    call(
+      "POST",
+      "https://skylabcoders.herokuapp.com/api/v2/users/auth",
+      `{ "username": "${email}", "password": "${password}" }`,
+      { "Content-type": "application/json" },
+      (error, status, body) => {
+        if (error) return done(error);
+        if (status !== 200)
+          return done(new Error(`unexpected status ${status}`));
+
+        const { token } = JSON.parse(body);
+
+        call(
+          "DELETE",
+          "https://skylabcoders.herokuapp.com/api/v2/users",
+          `{ "password": "${password}" }`,
+          {
+            "Content-type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          (error, status, body) => {
+            if (error) return done(new Error(error.message));
+            if (status !== 204)
+              return done(new Error(`undexpected status ${status}`));
+
             done();
           }
         );
       }
     );
-  });
-  
-
-
-    it("should find all users and create an array.", (done) => {
-      debugger
-      searchUsers("helena", tokenValue, () => {
-      
-          expect(users).to.exist;
-          expect(users.length).to.be.greaterThan(0);
-          done();
-        }
-      );
-    });
-
-  // afterEach((done) => {
-  //   call(
-  //     "POST",
-  //     "https://skylabcoders.herokuapp.com/api/v2/users/auth",
-  //     `{ "username": "${email}", "password": "${password}" }`,
-  //     { "Content-type": "application/json" },
-  //     (error, status, body) => {
-  //       if (error) return done(error);
-  //       if (status !== 200)
-  //         return done(new Error(`unexpected status ${status}`));
-
-  //       const { token } = JSON.parse(body);
-
-  //       call(
-  //         "DELETE",
-  //         "https://skylabcoders.herokuapp.com/api/v2/users",
-  //         `{ "password": "${password}" }`,
-  //         {
-  //           "Content-type": "application/json",
-  //           Authorization: `Bearer ${token}`,
-  //         },
-  //         (error, status, body) => {
-  //           if (error) return done(new Error(error.message));
-  //           if (status !== 204)
-  //             return done(new Error(`undexpected status ${status}`));
-
-  //           done();
-  //         }
-  //       );
-  //     }
-  //   );
-  // });
+  }); */
 });
