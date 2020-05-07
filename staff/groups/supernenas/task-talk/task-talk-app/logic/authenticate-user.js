94e77cb674fc1b0d4ae30c45b9e6a6ed1eb30149
traskimonias <graciaarriazuhector@gmail.com>
2020-05-07 13:24:17 +0200

    add authentication logic

---
+++ staff/groups/supernenas/task-talk/task-talk-app/logic/authenticate-user.js
@@ -0,0 +1,18 @@
+//Crea un pop up para que el usuario inicie sesión en trello
+function authenticateUser(onSuccess,onFailure){
+  //Comprueba que se le han mandado dos callbacks
+  if(typeof onSuccess!=="function") throw new TypeError(onSuccess+ " is not a function");
+  if(typeof onFailure!=="function") throw new TypeError(onFailure+ " is not a function");
+  
+  //Crea el popup de trello para que el usuario inicie sesion y llama a la callback correspondiente
+  window.Trello.authorize({
+    type: 'popup',
+    name: 'Getting Started Application',
+    scope: {
+      read: 'true',
+      write: 'true' },
+    expiration: '1hour',
+    success: onSuccess,
+    error: onFailure
+  });
+}
