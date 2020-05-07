4629068ea6c47824d382921eae403c0974ffbd47
Aitor1111 <lonesniper11@gmail.com>
2020-05-07 18:32:37 +0200

    start react environment

---
+++ staff/groups/team-jalapeno/cryptonite/components/App.jsx
@@ -0,0 +1,20 @@
+const { useState, Component } = React
+
+function App() {
+    const [view, setView] = useState('landing')
+
+
+
+    return
+    // LANDING
+    // NAV
+    // BRAND
+    // CRYPTO COINS 
+    // FOOTER
+
+    // REGISTER
+
+    // LOGIN
+
+    //HOME
+}
