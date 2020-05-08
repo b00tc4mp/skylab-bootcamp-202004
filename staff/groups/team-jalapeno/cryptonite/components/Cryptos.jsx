daae5c4be580c7a737219b5db4b9880fc57988c1
Aitor1111 <lonesniper11@gmail.com>
2020-05-08 17:58:18 +0200

    add cryptos compo working

--- staff/groups/team-jalapeno/cryptonite/components/Cryptos.jsx
+++ staff/groups/team-jalapeno/cryptonite/components/Cryptos.jsx
@@ -1,44 +1,47 @@
-const { useState , useEffect} = React
+const { useState, useEffect } = React
 
-function Cryptos({cryptoResults}){
+function Cryptos({ cryptoResults }) {
 
     const getChangePrice24Hr = (price, changePercent) => {
-        let result = (price * (1 - (changePercent / 100)).toFixed(3))
+        let result = (price / 100 * changePercent)
+        result = result.toFixed(2)
         result > 0 ? result = '+' + result : result.toString()
         return result
     }
 
-    return(
+    return (
         <section className="crypto-coins">
             <h3 className="crypto-coins__title">Crypto Coins</h3>
             <section className="crypto-coins__search">
                 <input type="text" placeholder="Find your next crypto favorite" />
                 <i className="fa fa-search " />
             </section>
-            <section className='crypto-container'>
+            <section className='coins-container'>
 
-            {!cryptoResults && <p>Loading...</p>}
-            {cryptoResults && cryptoResults.map(({name, symbol, rank, priceUsd, changePercent24Hr}) => {
+                {!cryptoResults && <p>Loading...</p>}
+                {cryptoResults && cryptoResults.map(({ name, symbol, rank, priceUsd, changePercent24Hr }) => {
 
-                {priceUsd = Number(priceUsd)
-                    priceUsd = priceUsd.toFixed(3).toString(); 
-                    changePercent24Hr = Number(changePercent24Hr);
-                    changePercent24Hr = changePercent24Hr.toFixed(3).toString()}
+                    {
+                        priceUsd = Number(priceUsd)
+                        priceUsd = priceUsd.toFixed(2).toString();
+                        changePercent24Hr = Number(changePercent24Hr);
+                        changePercent24Hr = changePercent24Hr.toFixed(2).toString()
+                    }
 
 
-                return <div className="coin">
-                    <div className="coin__head">
-                        <span className="coin__symbol">{symbol}</span>
-                        <span className="coin__rank">{rank}</span>
+                    return <div className="coin">
+                        <div className="coin__head">
+                            <span className="coin__symbol">{symbol}</span>
+                            <span className="coin__rank">{rank}</span>
+                        </div>
+                        <div className="coin__body">
+                            <span className="coin__name">{name}</span>
+                            <span className="coin__price">{priceUsd} USD</span>
+                            <span className="coin__percentage">{getChangePrice24Hr(priceUsd, changePercent24Hr)}$ ({changePercent24Hr}%)</span>
+                        </div>
                     </div>
-                    <div className="coin__body">
-                        <span className="coin__name">{name}</span>
-                        <span className="coin__price">{priceUsd} USD</span>
-                        <span className="coin__percentage">{getChangePrice24Hr(priceUsd, changePercent24Hr)}$ ({changePercent24Hr}%)</span>
-                    </div>
-                </div>  
-            })}
-        
+                })}
+
             </section>
         </section>
     )
