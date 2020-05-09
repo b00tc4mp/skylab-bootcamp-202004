a2a3b174d690c3cba9cf45f418c751b988b98d9e
polpedro <pol.pedro97@gmail.com>
2020-05-09 12:45:18 +0200

    add changes

--- staff/groups/4thElement/app/components/app.jsx
+++ staff/groups/4thElement/app/components/app.jsx
@@ -40,7 +40,8 @@ function App() {
             {view === 'register' && <Register onRegister={handleRegister} onGoToLogin={handleGoToLogin}/>}
             {view === 'login' && <Login onLogin={handleLogin} onGoToRegister={handleGoToRegister} onGoToHome={handleGoToHome}/>}
             {view === 'home' && <Home onGoToRegister={handleGoToRegister} onGoToLogin={handleGoToLogin} />}
-            {view === 'home' && <Footer/>}
+            {view === 'search' && <Search token={token} OnLogout={handleLogout}/>}
+            {view === 'home' && <Footer GoToSearch={handleGoToSearch}/>}
         </>
     
 }
--- staff/groups/4thElement/app/components/footer.jsx
+++ staff/groups/4thElement/app/components/footer.jsx
@@ -1,8 +1,13 @@
-function Footer () {
+function Footer ({GoToSearch}) {
+
+    const handleGoToSearch= () => {
+        GoToSearch()
+    }
+
     return <footer className="Footer">
     <section className="Footer__container">
         <i className="fas fa-home fa-2x"></i>
-        <i className="fas fa-search fa-2x"></i>
+        <i className="fas fa-search fa-2x" onClick={handleGoToSearch}></i>
         <i className="fas fa-map-marked-alt fa-2x"></i>
         <i className="fas fa-star fa-2x"></i>
     </section>
