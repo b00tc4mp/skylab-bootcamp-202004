b2a4204fc69381267e25ecf932a781fd888f604d
Aitor1111 <lonesniper11@gmail.com>
2020-05-09 11:28:01 +0200

    add coincard component

--- staff/groups/team-jalapeno/cryptonite/components/Cryptos.jsx
+++ staff/groups/team-jalapeno/cryptonite/components/Cryptos.jsx
@@ -2,12 +2,6 @@ const { useState, useEffect } = React
 
 function Cryptos({ cryptoResults, handleSearchOnChange }) {
 
-    const getChangePrice24Hr = (price, changePercent) => {
-        let result = (price / 100 * changePercent)
-        result = result.toFixed(2)
-        result > 0 ? result = '+' + result : result.toString()
-        return result
-    }
 
     return (
         <section className="crypto-coins">
@@ -29,17 +23,18 @@ function Cryptos({ cryptoResults, handleSearchOnChange }) {
                     }
 
 
-                    return <div className="coin" key={rank}>
-                        <div className="coin__head">
-                            <span className="coin__symbol">{symbol}</span>
-                            <span className="coin__rank">{rank}</span>
-                        </div>
-                        <div className="coin__body">
-                            <span className="coin__name">{name}</span>
-                            <span className="coin__price">{priceUsd} USD</span>
-                            <span className="coin__percentage">{getChangePrice24Hr(priceUsd, changePercent24Hr)}$ ({changePercent24Hr}%)</span>
-                        </div>
-                    </div>
+                    return <CoinCard rank={rank} symbol={symbol} name={name} priceUsd={priceUsd} changePercent24Hr={changePercent24Hr} />
+                    // <div className="coin" key={rank}>
+                    //         <div className="coin__head">
+                    //             <span className="coin__symbol">{symbol}</span>
+                    //             <span className="coin__rank">{rank}</span>
+                    //         </div>
+                    //         <div className="coin__body">
+                    //             <span className="coin__name">{name}</span>
+                    //             <span className="coin__price">{priceUsd} USD</span>
+                    //             <span className="coin__percentage">{getChangePrice24Hr(priceUsd, changePercent24Hr)}$ ({changePercent24Hr}%)</span>
+                    //         </div>
+                    //     </div>
                 })}
 
             </section>
