2da782d89c8fecc2ed69d9c019a4d2ceed2e00db
Juan David Om <anvidmen@gmail.com>
2020-05-10 14:36:15 +0200

    correction tweet.js

--- staff/juandavid-omen/web-components/my-app-react/logic/tweet.js
+++ staff/juandavid-omen/web-components/my-app-react/logic/tweet.js
@@ -2,11 +2,7 @@ function createTweet(token, message, callback) {
     String.validate.notVoid(token);
     String.validate.alphabetic(message);
     Function.validate(callback);
-
-   /*  if (typeof message !== 'string') throw new TypeError(message + ' is not a string')
-    if (!TEXT_REGEX.test(message)) throw new Error(message + ' is not alphabetic')
-    if (typeof callback !== 'function') throw new TypeError(`${callback} is not a function`)
- */
+    
     let newDate = new Date;
     newDate = newDate.toDateString();
     call('GET', 'https://skylabcoders.herokuapp.com/api/v2/users', undefined,
@@ -15,7 +11,6 @@ function createTweet(token, message, callback) {
             if (error) return callback(error)
 
             if (status === 200) {
-                callback()
                 const bodyObj = JSON.parse(body)
                 const { username } = bodyObj
                 const newTweet = { username, message, newDate }
