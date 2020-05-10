0b1cc47c9ac301843edac27c484fd2ae1eb40097
mac <mac@MacBook-Pro-de-mac.local>
2020-05-10 22:41:37 +0200

    sport news first approach

--- staff/groups/this.tintos/footify/footify-app/components/Home.jsx
+++ staff/groups/this.tintos/footify/footify-app/components/Home.jsx
@@ -1,41 +1,50 @@
 const { useState } = React
 
-function Home({token}) {
+function Home({ token }) {
 
     const [view, setView] = useState()
     const [players, setPlayers] = useState()
     const [error, setError] = useState()
     const [emblem, setEmblem] = useState()
-    const [likes,setLikes] = useState()
+    const [likes, setLikes] = useState()
+    const [sportNews, setSportNews] = useState()
 
 
     const handleGoToPlayerResults = (queryPlayer) => {
         try {
             searchPlayers(queryPlayer, (error, resultsPlayer) => {
                 if (error) return SetError(error.message)
-                
+
                 setPlayers(resultsPlayer)
-                
+
                 searchPlayersLikes(resultsPlayer, token, (error, resultLikes) => {
                     if (error) return SetError(error.message)
 
                     setLikes(resultLikes)
                 })
-               
                 setView('cards')
-
             })
-
-
         } catch ({ message }) {
             setError(message)
         }
     }
+    const handleGoToSport = () => {
+        searchSport((listResults) => {
+
+            setSportNews(listResults)
+            setView('sport')
+
+        })
+
+
+
+    }
 
     return <>
-        <Navbar onGoToPlayerResults={handleGoToPlayerResults} />
+        <Navbar onGoToPlayerResults={handleGoToPlayerResults} onGoToSportNews={handleGoToSport} />
 
-        {view === 'cards' && <PlayerResults resultsPlayers={players} resultLikes={likes}/>}
+        {view === 'cards' && <PlayerResults resultsPlayers={players} resultLikes={likes} />}
+        {view === 'sport' && <SportNews sportNews={sportNews}/>}
         {error && <Feedback message={error} level="error" />}
     </>
 
---
+++ staff/groups/this.tintos/footify/footify-app/logic/search-sport.js
@@ -0,0 +1,33 @@
+function searchSport(callback) {
+
+    let listResults = []
+    var xhr = new XMLHttpRequest()
+
+    xhr.open('GET', `https://skylabcoders.herokuapp.com/proxy?url=https://www.sport.es`)
+
+    xhr.onload = function () {
+
+        const parser = new DOMParser()
+
+        const doc = parser.parseFromString(this.responseText, 'text/html')
+
+        const results = doc.querySelectorAll('.sp-noticia')
+        
+        for (var i = 0; i < 10; i++) {
+            const title = results[i].querySelector('.title > a').innerHTML
+
+            const { href: link } = results[i].querySelector('.title > a')
+
+            const { src: linkImg } = results[i].querySelector('img')
+
+            listResults.push({ title, link, linkImg })
+        }
+
+        callback(listResults)
+    }
+
+    xhr.onerror = function (error) {
+        console.error('KO', error.message)
+    }
+    xhr.send()
+}
