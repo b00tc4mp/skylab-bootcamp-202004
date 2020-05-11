5ebc867938f1825a35017c7631bdf3ed912bdf25
betho <vetu87@gmail.com>
2020-05-11 18:18:05 +0200

    update checkbox logic

--- staff/groups/equipo-a/clear-news/clear-news-app/components/Profile.jsx
+++ staff/groups/equipo-a/clear-news/clear-news-app/components/Profile.jsx
@@ -1,12 +1,36 @@
-const { useState,useEffect } = React
+const { useState, useEffect } = React
 
-function Profile({ onChangeProf,token}) {
+function Profile({ onChangeProf, token }) {
     const [error, setError] = useState()
+    //todo a traves de estado setear variables
+
+    // useEffect(() => {
+    //     retrieveUser(token, (error,  { name, surname, username, categories, country}) => {
+    //         let user={name, surname, username, categories, country}
+    //         if (error) setError(error.message)
+    //         const form=document.getElementsByClassName('register__input')
+    //         form[0].innerText= name
+    //         // form[1].innerText= surname
+    //         country=turnToCountry(country)
+    //         document.getElementsByClassName('register__country')[0].options[country].selected=true
+    //         const checkbox=document.querySelectorAll('input[type=checkbox]')
+    //         let result = Object.keys(categories).map(function (key) { return  categories[key] })
+    //            for(let i=0; i<result.length; i++){
+    //                if(result){
+    //                    let j=i
+    //                 for(j=0;j<checkbox.length;j++){
+                        
+    //                     checkbox[j].checked=true
+    //                 }
+    //             }
+    //            }
+    //     })
+    // }, [])
 
     const handleSubmit = event => {
         event.preventDefault()
-        let country
-        let business =false
+        
+        let business = false
         let entertainment = false
         let general = false
         let health = false
@@ -14,7 +38,7 @@ function Profile({ onChangeProf,token}) {
         let sports = false
         let technology = false
 
-        
+
         if (event.target.business.checked) business = Boolean(event.target.business.value)
         if (event.target.entertainment.checked) entertainment = Boolean(event.target.entertainment.value)
         if (event.target.general.checked) general = Boolean(event.target.general.value)
@@ -25,170 +49,84 @@ function Profile({ onChangeProf,token}) {
 
         let interests = { business, entertainment, general, health, science, sports, technology }
 
-        let { name, surname, email, password,oldPassword } = event.target
+        let { name, surname, email, password, oldPassword } = event.target
         debugger
         name = name.value
         surname = surname.value
         email = email.value
         password = password.value
-        oldPassword= oldPassword.value
+        oldPassword = oldPassword.value
 
-        switch (event.target.country.value) {
-            case 'Arab Emirates':
-                country = "ea"
-                break;
-            case 'Argentina':
-                country = "ar"
-                break;
-            case 'Austria':
-                country = "at"
-                break;
-            case 'Australia':
-                country = "au"
-                break;
-            case 'Belgica':
-                country = "be"
-                break;
-            case 'Bulgaria':
-                country = "bg"
-                break;
-            case 'Brasil':
-                country = "br"
-                break;
-            case 'Canada':
-                country = "ca"
-                break;
-            case 'Switzerland':
-                country = "ch"
-                break;
-            case 'China':
-                country = "cn"
-                break;
-            case 'Colombia':
-                country = "co"
-                break;
-            case 'Cuba':
-                country = "cu"
-                break;
-            case 'Czch Republica':
-                country = "cz"
-                break;
-            case 'Germany':
-                country = "de"
-                break;
-            case 'France':
-                country = "fr"
-                break;
-            case 'Great Britain':
-                country = "gb"
-                break;
-            case 'Greece':
-                country = "gr"
-                break;
-            case 'Hong Kong':
-                country = "hk"
-                break;
-            case 'Hungary':
-                country = "hu"
-                break;
-            case 'Irlanda':
-                country = "ie"
-                break;
-            case 'Israel':
-                country = "il"
-                break;
-            case 'Italy':
-                country = "it"
-                break;
-            case 'Japan':
-                country = "jp"
-                break;
-            case 'Lituania':
-                country = "lt"
-                break;
-            case 'Letonia':
-                country = "lv"
-                break;
-            case 'Marocco':
-                country = "ma"
-                break;
-            case 'Mexico':
-                country = "mx"
-                break;
-            case 'Malasia':
-                country = "my"
-                break;
-            case 'Nigeria':
-                country = "ng"
-                break;
-            case 'Netherlands':
-                country = "nl"
-                break;
-            case 'Norway':
-                country = "no"
-                break;
-            case 'New Zealand':
-                country = "nz"
-                break;
-            case 'Phillipines':
-                country = "ph"
-                break;
-            case 'Polonia':
-                country = "pl"
-                break;
-            case 'Portugal':
-                country = "pt"
-                break;
-            case 'Rumania':
-                country = "ro"
-                break;
-            case 'Serbia':
-                country = "rs"
-                break;
-            case 'Russia':
-                country = "ru"
-                break;
-            case 'Saudi Arabia':
-                country = "sa"
-                break;
-            case 'Sweden':
-                country = "se"
-                break;
-            case 'Singapur':
-                country = "sg"
-                break;
-            case 'Eslovenia':
-                country = "si"
-                break;
-            case 'Thailand':
-                country = "th"
-                break;
-            case 'Turkey':
-                country = "tr"
-                break;
-            case 'Taiwan':
-                country = "tw"
-                break;
-            case 'Ucrania':
-                country = "ua"
-                break;
-            case 'United States of America':
-                country = "us"
-                break;
-            case 'Venezuela':
-                country = "ve"
-                break;
-            case 'South Africa':
-                country = "za"
-                break;
-            case 'Default':
-                country = null
-                break;
-            default:
-                throw error
-        }
+        // let country=turnToIata(event.target.country.value)
+        // switch (event.target.country.value) {
+        //     case 'International News':
+        //         country = ""
+        //         break;
+        //     case 'Argentina':
+        //         country = "ar"
+        //         break;
+        //     case 'Austria':
+        //         country = "at"
+        //         break;
+        //     case 'Australia':
+        //         country = "au"
+        //         break;
+        //     case 'Brasil':
+        //         country = "br"
+        //         break;
+        //     case 'Canada':
+        //         country = "ca"
+        //         break;
+        //     case 'China':
+        //         country = "cn"
+        //         break;
+        //     case 'Germany':
+        //         country = "de"
+        //         break;
+        //     case 'France':
+        //         country = "fr"
+        //         break;
+        //     case 'Great Britain':
+        //         country = "gb"
+        //         break;
+        //     case 'Hong Kong':
+        //         country = "hk"
+        //         break;
+        //     case 'Irlanda':
+        //         country = "ie"
+        //         break;
+        //     case 'Italy':
+        //         country = "it"
+        //         break;
+        //     case 'Japan':
+        //         country = "jp"
+        //         break;
+        //     case 'Marocco':
+        //         country = "ma"
+        //         break;
+        //     case 'Mexico':
+        //         country = "mx"
+        //         break;
+        //     case 'New Zealand':
+        //         country = "nz"
+        //         break;
+        //     case 'Phillipines':
+        //         country = "ph"
+        //         break;
+        //     case 'Portugal':
+        //         country = "pt"
+        //         break;
+        //     case 'Russia':
+        //         country = "ru"
+        //         break;
+        //     case 'United States of America':
+        //         country = "us"
+        //         break;
+        //     default:
+        //         throw error
+        
         try {
-            profileChange(token, name, surname, email, password,oldPassword, interests, country, error => {
+            profileChange(token, name, surname, email, password, oldPassword, interests, country, error => {
                 if (error) return setError(error.message)
 
                 onChangeProf()//aqui meter los valores que queremos pasar a home
@@ -201,78 +139,49 @@ function Profile({ onChangeProf,token}) {
 
     return <section className="register">
         <h1>Change profile</h1>
-        <form className="register__input" autoComplete ="off" onSubmit={handleSubmit}>
-            <input type="text" name="name" placeholder="name" required pattern="[A-Za-z]{1,20}" />
-            <input type="text" name="surname" placeholder="surname" required pattern="[A-Za-z]{1,20}" />
-            <input type="email" name="email" placeholder="e-mail"  required />
-            <input type="password" name="password" placeholder="password"   required minLength="8" />
+        <form className="register__input" onSubmit={handleSubmit}>
+            {name} = <input type="text" name="name" placeholder= "name" required pattern="[A-Za-z]{1,20}" />
+            <input type="text" name="surname" placeholder="username" required pattern="[A-Za-z]{1,20}" />
+            <input type="email" name="email" placeholder="e-mail" required />
+            <input type="password" name="password" placeholder="password" required minLength="8" />
             <input type="password" name="oldPassword" placeholder="Old password" required minLength="8" />
 
             <label >Change your country:</label>
             <select className="register__country" name="country">
-                    <option>Default</option>
-                    <option>Arab Emirates</option> 
-                    <option>Argentina</option> 
-                    <option>Austria</option>  
-                    <option>Australia</option> 
-                    <option>Belgica</option>
-                    <option>Bulgaria</option>
-                    <option>Brasil</option>
-                    <option>Canada</option>
-                    <option>Switzerland</option>
-                    <option>China</option>
-                    <option>Colombia</option>
-                    <option>Cuba</option>
-                    <option>Czch Republica</option>
-                    <option>Germany</option>
-                    <option>France</option>
-                    <option>Great Britain</option>
-                    <option>Greece</option>
-                    <option>Hong Kong</option>
-                    <option>Hungary</option>
-                    <option>Irlanda</option>
-                    <option>Israel</option>
-                    <option>Italy</option>
-                    <option>Japan</option>
-                    <option>Lituania</option>
-                    <option>Letonia</option>
-                    <option>Marocco</option>
-                    <option>Mexico</option>
-                    <option>Malasia</option>
-                    <option>Nigeria</option>
-                    <option>Netherlands</option>
-                    <option>Norway</option>
-                    <option>New Zealand</option>
-                    <option>Phillipines</option>
-                    <option>Polonia</option>
-                    <option>Portugal</option>
-                    <option>Rumania</option>
-                    <option>Serbia</option>
-                    <option>Russia</option>
-                    <option>Saudi Arabia</option>
-                    <option>Sweden</option>
-                    <option>Singapur</option>
-                    <option>Eslovenia</option>
-                    <option>Thailand</option>
-                    <option>Turkey</option>
-                    <option>Taiwan</option>
-                    <option>Ucrania</option>
-                    <option>United States of America</option>
-                    <option>Venezuela</option>
-                    <option>South Africa</option>
+                <option>International News</option>
+                <option>Argentina</option>
+                <option>Austria</option>
+                <option>Australia</option>
+                <option>Brasil</option>
+                <option>Canada</option>
+                <option>China</option>
+                <option>Germany</option>
+                <option>France</option>
+                <option>Great Britain</option>
+                <option>Hong Kong</option>
+                <option>Irlanda</option>
+                <option>Italy</option>
+                <option>Japan</option>
+                <option>Marocco</option>
+                <option>Mexico</option>
+                <option>New Zealand</option>
+                <option>Phillipines</option>
+                <option>Portugal</option>
+                <option>Russia</option>
+                <option>United States of America</option>
             </select>
             <fieldset className="register__checkbox" required>
                 <legend>Change your favorite topics</legend>
-                <input type="checkbox" name="business" value="true" />Business<br/>
-                <input type="checkbox" name="entertainment" value="true" />Entertainment<br/>
-                <input type="checkbox" name="general" value="true" />General<br/>
-                <input type="checkbox" name="health" value="true" />Health<br/>
-                <input type="checkbox" name="science" value="true" />Science<br/>
-                <input type="checkbox" name="sports" value="true" />Sports<br/>
-                <input type="checkbox" name="technology" value="true" />Technology<br/>
+                <input type="checkbox" name="business" value="true" />Business<br />
+                <input type="checkbox" name="entertainment" value="true" />Entertainment<br />
+                <input type="checkbox" name="general" value="true" />General<br />
+                <input type="checkbox" name="health" value="true" />Health<br />
+                <input type="checkbox" name="science" value="true" />Science<br />
+                <input type="checkbox" name="sports" value="true" />Sports<br />
+                <input type="checkbox" name="technology" value="true" />Technology<br />
             </fieldset>
             <section className="resister__nav-button">
-            <button className="register__button" >Apply</button>
+                <button className="register__button" >Apply</button>
             </section>
             {error && <Feedback message={error} level="error" />}
         </form>
--- staff/groups/equipo-a/clear-news/clear-news-app/logic/register-user.js
+++ staff/groups/equipo-a/clear-news/clear-news-app/logic/register-user.js
@@ -1,29 +1,40 @@
-function registerUser(name, surname, email, password, categories, country, callback) {
+function registerUser(name, surname, email, password, interests, country, callback) {
+
     String.validate.alphabetic(name)
+    String.validate.notVoid(name)
     String.validate.alphabetic(surname)
-
+    String.validate.notVoid(surname)
     Email.validate(email)
-
     String.validate.lengthGreaterEqualThan(password, 8)
-
-    String.validate.alphabetic(country)
-
     Function.validate(callback)
 
-
-    call('POST', 'https://skylabcoders.herokuapp.com/api/v2/users',
-        `{"name": "${name}", "surname": "${surname}", "username": "${email}", "password": "${password}", "categories": ${JSON.stringify(categories)}, "country": "${country}"}`,
-        { 'Content-type': 'application/json' },
-        (error, status, body) => {
-            if (error) return callback(error)
-
-            if (status === 201) callback()
-            else {
-                const { error } = JSON.parse(body)
-
-                callback(new Error(error))
-            }
+    let count = 0
+    for (interest in interests) {
+        if (interests[interest] === false) {
+            count++
         }
-    )
+    }
+
+    if (count === 7) {
+        callback(new Error('At least one field should be checked'))
+    } else {
+
+        call('POST', 'https://skylabcoders.herokuapp.com/api/v2/users',
+            `{"name": "${name}", "surname": "${surname}", "username": "${email}", "password": "${password}", "categories": ${JSON.stringify(interests)}, "country": "${country}"}`,
+            { 'Content-type': 'application/json' },
+            (error, status, body) => {
+                if (error) return callback(error)
+
+                if (status === 201) callback()
+                else {
+                    const { error } = JSON.parse(body)
+
+                    callback(new Error(error))
+                }
+            }
+        )
+
+    }
+
 
 }
