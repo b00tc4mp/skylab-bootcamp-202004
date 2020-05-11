cc244bdd85354ab8bc1affc73a502423c9b07208
sergoustar <sergirutu@gmail.com>
2020-05-11 17:30:26 +0200

    added create new activity

---
+++ staff/groups/supernenas/task-talk/task-talk-app/logic/create-new-activity.js
@@ -0,0 +1,11 @@
+//Crea una nueva actividad vacia
+function createNewActivity(activityName, listId, onSuccess, onFailure) {
+    //Comprueba que se le han mandado dos callbacks
+    Function.validate(onSuccess);
+    Function.validate(onFailure);
+
+    String.validate(activityName);
+    String.validate(listId);
+
+    Trello.post("cards", { name: activityName, idList: listId }, onSuccess, onFailure);
+}
