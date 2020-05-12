f7825363407d03c587e858dbff2f0c41ff711bf3
alejandromc23 <alejandro.mascort17@hotmail.com>
2020-05-12 16:12:53 +0200

    adapt to BEM

--- staff/groups/orlando-magic/card-search/app/components/UserCard.jsx
+++ staff/groups/orlando-magic/card-search/app/components/UserCard.jsx
@@ -1,19 +1,15 @@
 function UserCard({nickname, email, myCards}) {
     return <>
-        <div className='user-card-container'>
-            <section className="menu-title">
-                <h1 className='name title'>{nickname}</h1>
-                <div className="frame-art-container">
-                    <img className='frame-art' src='https://i.blogs.es/eadad8/chandra/450_1000.jpg' alt='jace art'/>
+        <div class='user-card-container'>
+            <section class="card-container__section">
+                <h1 class='card-container__name card-container__title'>ALex}</h1>
+                <div class="card-container__frame-art-container">
+                    <img class='card-container__frame-art' src='https://i.blogs.es/eadad8/chandra/450_1000.jpg' alt='jace art'/>
                 </div>
-                <h1 className="type title">{email}</h1>
-                <div className="form-container">
-                    <form className='form' >
-                        <h2>{nickname} has <a href="">34</a> favourite cards.</h2>
-                    </form>
-                    <div className="nav">
-                        <a className="nav__item" href=""><h1>Follow</h1></a>
-                    </div>
+                <h1 class="tcard-container__type card-container__title">alexxx</h1>
+                <div class="card-container__nav">
+                    <a class="card-container__nav-item" href=""><h1>Follow</h1></a>
+                    <a class="card-container__nav-item" href=""><h1>View Profile</h1></a>
                 </div>
             </section>
         </div>
