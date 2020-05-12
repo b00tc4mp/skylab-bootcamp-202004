1b9ec4886a928e36c5ec1517ce07c70e20b4a8e3
mac <mac@MacBook-Pro-de-mac.local>
2020-05-12 16:27:27 +0200

    template fwitter done i think :)

--- staff/groups/this.tintos/footify/footify-app/components/Fwitter.jsx
+++ staff/groups/this.tintos/footify/footify-app/components/Fwitter.jsx
@@ -1,3 +1,5 @@
+const {useState} = React
+
 function Fwitter({fwitter}){
     const _fwitter = fwitter
     
@@ -33,22 +35,24 @@ function Fwitter({fwitter}){
                 { arrfwitter.map(({idUser,nameUser,surnameUser,name,message,date})=>
                                 <div className='fwitter__user'>
                                     <div className='fwitter__user-item'>
-                                        <div className='fwitter__point'></div>
-                                        <div className='fwitter__user-name'>{`${nameUser} ${surnameUser}`}</div>
+                                        <img className='fwitter__point' src="img/boy.svg" alt=""/>
+                                        <div className='fwitter__user-name'>{`@${nameUser} ${surnameUser}`}</div>
                                     </div>
+
                                     <div className='fwitter__comment'>
-                                    <div className='fwitter__player-name'>{`${name}`}</div>
-                                    <div className=''>
-                                        <div className='fwitter__message'>{message}</div>
-                                        <div className='fwitter__date'>{date}</div>
-                                            <div class='fwitter__card-container'>
-                                                <div class='fwitter__card'></div>
-                                                <div class='fwitter__card'></div>
-                                                <div class='fwitter__card'></div>
-                                            </div>
+                                    <div className='fwitter__player-name'>{`@${name} `}<span className='fwitter__message'>{message}</span></div>
+                                        <div className=''>
+                                            <div className='fwitter__date'>{date}</div>
+                                                <div className='fwitter__card-container'>
+                                                    <a href=""><img className="fwitter__card-img" src="img/card_red.svg" alt=""/></a>
+                                                    <a href=""><img className="fwitter__card-img" src="img/card_yelow.svg" alt=""/></a>
+                                                    <a href=""><img className="fwitter__card-img" src="img/card_gren.svg" alt=""/></a>
+                                                   
+                                                </div>
                                  
+                                        </div>
                                     </div>
-                                    </div>
+
                                 </div>
                             )}<div >
                     </div>
