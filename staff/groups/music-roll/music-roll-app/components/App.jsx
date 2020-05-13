b5947fd11f74ccd2a71631b4fad00a8f8b8bb529
LLorch1984 <jorgegenove@gmail.com>
2020-05-13 10:50:13 +0200

    album styles

--- staff/groups/music-roll/music-roll-app/components/App.jsx
+++ staff/groups/music-roll/music-roll-app/components/App.jsx
@@ -1,44 +1,44 @@
 const { Component } = React
 
 class App extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
 
         this.state = {
-            view: 'home', 
+            view: 'home',
 
-            token: "BQDj85_ESBdFATHHkRTgRiKenuuzbHzks13lwymWotcfH-2TMMr8N2pNTrdJsgR917YVxSki8Ui7i1HhA7CpaDNkq6bbxw8CoC83poIvWQQO9Zaxbx1X0mugCVgJ1BAEjQsIqLkzm8o2YlpUDPP_DcQGgwh-UcSwQEhKGMub30nIIrHsgaFp3pE6wgNh"
+            token: "BQA9mJ03s3gLUDqZY1hktBkvaY5nT_DZVt6BD0Kz-5kbvDJKJz0hEySTnAQi7UXqWeKGP6tlXhQsbrVjFDRg5rHq5czCTMbnSm_XXdSaCIysoQSglTLESG_vR959uFxUL-ptTOZw5SunXAdP-DV_5Iyh3RIq2cP1fs9obCz2yXZsrdi6xpkg_v59PogB"
 
-         
+
 
         }
     }
 
 
-    onChangeView = (_view) => this.setState({view : _view})
+    onChangeView = (_view) => this.setState({ view: _view })
 
     handleLogin = (_token) => {
-        this.setState({token: _token})
-        
-         this.setState({view: 'home'})
+        this.setState({ token: _token })
+
+        this.setState({ view: 'home' })
     }
-    
+
     handleRegister = () => {
         this.onChangeView('login')
     }
 
-   
+
 
     render() {
         return <>
-            {this.state.view === 'login' && <Login onSubmit = {this.handleLogin} onRegister = {this.onChangeView}/>}
-            {this.state.view === 'register' && <Register onSubmit = {this.handleRegister} onLogin = {this.onChangeView} />}
+            {this.state.view === 'login' && <Login onSubmit={this.handleLogin} onRegister={this.onChangeView} />}
+            {this.state.view === 'register' && <Register onSubmit={this.handleRegister} onLogin={this.onChangeView} />}
 
-            <Navbar onChangeView={this.onChangeView}/>
+            <Navbar onChangeView={this.onChangeView} />
 
             {this.state.view === 'home' && <Home />}
-            {this.state.view === 'browser' && <Browser token = {this.state.token}/>}
-            
+            {this.state.view === 'browser' && <Browser token={this.state.token} />}
+
         </>
 
     }
