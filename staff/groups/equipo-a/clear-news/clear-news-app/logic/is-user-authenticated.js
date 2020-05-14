870c56b2434a1d5fc36233c18182ce63224b60b6
Ander Martin Ripa <andermartinr@gmail.com>
2020-05-14 19:51:56 +0200

    is-user-authenticate test

--- staff/groups/equipo-a/clear-news/clear-news-app/logic/is-user-authenticated.js
+++ staff/groups/equipo-a/clear-news/clear-news-app/logic/is-user-authenticated.js
@@ -12,13 +12,16 @@ function isUserAuthenticated(token, callback) {
     String.validate.notVoid(token)
 
     Function.validate(callback)
-
+  
     call('GET', 'https://skylabcoders.herokuapp.com/api/v2/users',
         undefined,
         { 'Authorization': `Bearer ${token}` },
-        (error, status) => {
+        (error, status,body) => {
             if (error) return callback(error)
-
+            if(status>=400){
+              const {error:_error}=JSON.parse(body)
+              return callback(new Error (_error))
+            }
             callback(undefined, status === 200)
         }
     )
