b475da3a5a1d6e85f64836bc7cf14e196bcc6815
jordigraup <59455949+jordigraup@users.noreply.github.com>
2020-05-18 15:12:29 +0200

    Update every.spec.js

--- staff/jordi-grau/arrays/array-functions/every.spec.js
+++ staff/jordi-grau/arrays/array-functions/every.spec.js
@@ -1,7 +1,7 @@
 'use strict'
 
 describe('every', function () {
-    it('delete the last index position of one array', function () {
+    it('should return a boolean, depending if all the elements of the array pass a specificated condition', function () {
         var array = [1,2,3,4,5,6,7];
 
         var result1 = every(array,function(element){
