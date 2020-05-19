d3db7e8f6c086e890fe29f5ea9396f4d0747b1ad
marcgm97 <mguillemas@gmail.com>
2020-05-19 18:39:12 +0200

    sockets save first approach

--- staff/marc-guillemas/node/contacts/components/SearchContacts.js
+++ staff/marc-guillemas/node/contacts/components/SearchContacts.js
@@ -1,12 +1,44 @@
-const searchContact = require('../logic/search-contacts')
-const readline = require('readline')
+const searchContact = require('../logic/search-contacts');
+const readline = require('readline');
+const net = require('net');
 
-function SearchContact() {
+(function SearchContact() {
     
     const interface = readline.Interface({
         input: process.stdin,
         output: process.stdout
     })
 
-    interface.question('')
-}
+    interface.question('Search for a user: ', query => {
+        searchContact(query, (error, contacts) => {
+            if(error) throw new Error(error)
+            if(contacts) interface.close()
+//m'instalo hangouts  bueno el pots obrir des
+            const server = net.createServer(socket => {
+                socket.on('data', data => {
+                    console.log(data)
+                    socket.write(`HTTP/1.1 200
+                    content-type: text/html
+                    
+                    <ul>
+                        ${contacts.forEach(({name, surname, phone, email, country}) => {
+                            `<li>${name},${surname},${phone},${email},${country}</li>`
+                            console.log("hello world")
+                        })}
+                    </ul>
+
+                    `)
+                    socket.end()
+                })
+
+                socket.on('error', console.log)
+
+            })
+            server.listen(8080, () => {
+                console.log("server start")
+            })
+            
+            
+        }) 
+    })
+}())
--- staff/marc-guillemas/node/contacts/logic/search-contacts.js
+++ staff/marc-guillemas/node/contacts/logic/search-contacts.js
@@ -5,14 +5,17 @@ function searchContacts(query, callback) {
     fs.readdir(path.join(__dirname,'..','data'), (error, files) => {
         if(error) throw new Error(error)
 
+        let cont = 0
         const contacts = []
         files.forEach(file => {
             fs.readFile(path.join(__dirname,'..','data',file), (error, data) => {
-                const obj = JSON.parse(data)
-                const check = value(obj)
-                check.includes(query) && contacts.push(obj)
+                if(error) throw new Error(error)
 
-                if(contacts.length === files.length) callback(null, contacts)
+                const obj = JSON.parse(data)
+                const values = Object.values(obj)
+                values.some(value => { value.includes(query)}) && contacts.push(obj)
+
+                if(++cont === data.length) callback(null, contacts)
             })
         })
     })
