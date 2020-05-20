4b4ffa99e6e61fcca5ca583abc69e3134b265b2e
alejandromc23 <alejandro.mascort17@hotmail.com>
2020-05-20 11:38:39 +0200

    add corrections

--- staff/alejandro-mascort/node/contacts/logic/extract-inputs.js
+++ staff/alejandro-mascort/node/contacts/logic/extract-inputs.js
@@ -3,12 +3,13 @@ module.exports = data =>  {
 
     let formInputs = data.split('\n')[data.split('\n').length-1]
     formInputs = formInputs.split('&')
-    formInputs = formInputs.map(input => input.substring(input.indexOf('=')+1))
-    contact.name = formInputs[0]
-    contact.surname = formInputs[1]
-    contact.email= formInputs[2].replace('%40', '@')
-    contact.phone = formInputs[3]
-    // formInputs.forEach(input => contact[input[0].trim()]=input[1].trim())
+    // formInputs = formInputs.map(input => input.substring(input.indexOf('=')+1))
+    // contact.name = formInputs[0]
+    // contact.surname = formInputs[1]
+    // contact.email= formInputs[2].replace('%40', '@')
+    // contact.phone = formInputs[3]
+    formInputs = formInputs.map(input => input.split('='))
+    formInputs.forEach(input => contact[input[0].trim()]=input[1].trim().replace('%40', '@'))
 
     
     return contact
