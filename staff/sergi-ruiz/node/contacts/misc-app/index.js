2c20c5e29229e7d9b7a62a835befc9e929b9fcd9
sergoustar <sergirutu@gmail.com>
2020-05-21 16:31:21 +0200

    ?

---
+++ staff/sergi-ruiz/node/contacts/misc-app/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const hello = require('./public/components/hello')
+const app = express()
+
+app.use(express.static('public'))
+
+app.get('/hello-world', (req, res) => {
+    res.send(`<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="UTF-8" />
+    <title>Hello World</title>
+    <link href="images/skylab-icon.png" type="image/png" rel="Shortcut Icon">
+    <link rel="stylesheet" href="style.css"> 
+    </head>
+    <body>
+        hola mundo!!!
+    </body>
+</html>`)
+})
+
+
+
+app.get('/hello', (req, res) => {
+    res.send(hello())
+})
+
+app.listen(8080)
---
+++ staff/sergi-ruiz/node/contacts/misc-app/public/components/hello.js
@@ -0,0 +1,12 @@
+
+module.exports = () => {
+    return `<section class="contacts">
+    <h2>Contacts list</h2>
+<ul>
+    <h1> hello guai </h1>
+</ul>
+</section>`
+}
+
+
+
