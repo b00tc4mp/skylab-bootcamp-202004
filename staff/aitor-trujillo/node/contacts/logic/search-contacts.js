cb4fe1aa4c12e27dd5af968054eb3af37243d975
Aitor1111 <lonesniper11@gmail.com>
2020-05-23 17:37:41 +0200

    update files

--- staff/aitor-trujillo/node/contacts/logic/search-contacts.js
+++ staff/aitor-trujillo/node/contacts/logic/search-contacts.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const path = require('path')
 
 module.exports = (query, callback) => {
-    fs.readdir(path.join(__dirname, '..', 'data'), (error, files) => {
+    fs.readdir(path.join(__dirname, '..', 'data', 'contacts'), (error, files) => {
         if (error) return callback(error)
 
         let wasError = false
@@ -11,7 +11,7 @@ module.exports = (query, callback) => {
         let count = 0
 
         files.forEach(file => {
-            fs.readFile(path.join(__dirname, '..', 'data', file), 'utf8', (error, json) => {
+            fs.readFile(path.join(__dirname, '..', 'data', 'contacts', file), 'utf8', (error, json) => {
                 if (error) {
                     if (!wasError) {
                         callback(error)
