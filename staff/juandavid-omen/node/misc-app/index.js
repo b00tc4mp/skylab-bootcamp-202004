fc06e3a4fac79c84442c22852fa9460f9a3e1913
Juan David Om <anvidmen@gmail.com>
2020-05-27 00:08:07 +0200

    modified index file

--- staff/juandavid-omen/node/misc-app/index.js
+++ staff/juandavid-omen/node/misc-app/index.js
@@ -1,24 +1,21 @@
 const express = require('express')
-const App = require('./components/App')
+const { App, Register, Login, Home, Landing } = require('./components')
+const { register, authenticateUser, retrieveUser } = require('./logic')
 const listContacts = require('./logic/list-contacts')
 const searchContacts = require('./logic/search-contacts')
 const addContact = require('./logic/add-contact')
 const ListContacts = require('./components/ListContacts')
 const SearchContacts = require('./components/SearchContacts')
 const AddContact = require('./components/AddContact')
-const Landing = require('./components/Landing')
-const fs = require('fs')
-const path = require('path')
 const Feedback = require('./components/Feedback')
 const objetize = require('./helper/objetize')
-const Register = require('./components/Register')
-const register = require('./logic/register')
-const Login = require('./components/Login')
-const authenticateUser = require('./logic/authenticate-user')
 
 const bodyParser = require('body-parser')
-
 const app = express()
+
+app.use(express.static('public'))
+
+
 app.use(bodyParser.urlencoded({
     extended: false
 }))
@@ -60,9 +57,7 @@ app.post('/add-contact', (req, res) => {
     })
 })
 
-app.get('/register', (req, res) => {
-    res.send(App(Register()))
-})
+app.get('/register', (req, res) => res.send(App(Register())))
 
 app.post('/register', (req, res) => {
     const { body } = req
@@ -74,13 +69,14 @@ app.post('/register', (req, res) => {
             res.send(App(Feedback("Fail:(", 'error')))
 
         } else {
-            res.send(res.send(App(Login)))
+            res.send(App(Login))
         }
     })
 })
 
 app.get('/login', (req, res) => {
     res.send(App(Login()))
+    
 })
 
 app.post('/login', (req, res) => {
@@ -89,62 +85,7 @@ app.post('/login', (req, res) => {
     authenticateUser(body, (error, emailFound)=> {
         if(error) throw error
 
-        !emailFound? res.send(App(Feedback("There was an error loging in"))) : res.send(App(Feedback("CONGRATS! LOGGED IN!")))
+        !emailFound? res.send(App(Feedback("There was an error loging in"))) : res.send(App(Home()))
     })
 })
-
-
-
-
-/*
-    } else if (url.startsWith('/search')) {
-        if (!url.includes('?')) {
-            res.end(App(SearchContacts()))
-        } else {
-            const [, queryString] = url.split('?')
-
-            const [, query] = queryString.split('=')
-
-            searchContacts(query, (error, contacts) => {
-                if (error) throw error
-
-                res.end(App(`${SearchContacts(query)}${ListContacts(contacts)}`))
-            })
-        }
-    } else if (url === '/add-contact') {
-        if (method === 'GET') {
-
-            res.end(App(AddContact()))
-
-        } else if (method==='POST'){
-            
-            req.on('data' , data =>{ 
-                
-                let obj = objetize(data, callback)
-                addContact(obj, (error, id)=>{
-                    const {name} = obj
-
-                    if (error) {
-                        res.end(App(Feedback("Fail:(",'error')))
-
-                    }else {
-                        res.end(App(Feedback(`Contact ${name} created!`)))
-                    }
-                })
-            })
-        }
-
-    } else if (url === '/style.css') {
-        fs.readFile(path.join(__dirname, url), 'utf8', (error, content) => {
-            if (error) throw error
-
-            res.setHeader('Content-Type', 'text/css')
-
-            res.end(content)
-        })
-    } else {
-
-    }
-})  */
-
 app.listen(8080)
