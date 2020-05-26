f55f82a6c28290a88a98db31d27d58b5338f6e96
Juan David Om <anvidmen@gmail.com>
2020-05-27 00:05:57 +0200

    modified add-contact file

--- staff/juandavid-omen/node/misc-app/logic/add-contact.js
+++ staff/juandavid-omen/node/misc-app/logic/add-contact.js
@@ -1,9 +1,9 @@
 const fs = require('fs')
 const path = require('path')
-require('../utils/string')
+require('../utils/polyfills/string')
 const Email = require('../utils/email')
 const uid = require('../utils/uid')
-require('../utils/json')
+require('../utils/polyfills/json')
 
 module.exports = (contact, callback) => {
     if (typeof contact !== 'object') throw new TypeError(`${contact} is not an object`)
