d16c9da12b5e6e76ea5567978ff23659ae1d188e
Juan David Om <anvidmen@gmail.com>
2020-05-26 12:06:37 +0200

    added anchor to Landing component

--- staff/juandavid-omen/node/misc-app/components/Landing.js
+++ staff/juandavid-omen/node/misc-app/components/Landing.js
@@ -1,12 +1,11 @@
 module.exports = () => {
-   return `<section class="contacts">
-    <h2>Contacts list</h2>
-<ul>
-   <li><a href="http://localhost:8080/add-contact" target="_blank">add contact</a></li>
-   <li><a href="http://localhost:8080/contacts" target="_blank">list contacts</a></li>
-   <li><a href="http://localhost:8080/search" target="_blank">search contacts</a></li>
-   <li><a href="http://localhost:8080/register" target="_blank">register</a></li>
-   <li><a href="http://localhost:8080/login" target="_blank">login</a></li>
-</ul>
+   return `<section class="landing">
+   <h2>Main</h2>
+   <a href="/register">Register</a> or <a href="/login">Login</a>
+   <ul>
+      <li><a href="http://localhost:8080/add-contact" target="_blank">add contact</a></li>
+      <li><a href="http://localhost:8080/contacts" target="_blank">list contacts</a></li>
+      <li><a href="http://localhost:8080/search" target="_blank">search contacts</a></li>
+   </ul>
 </section>`
 }
