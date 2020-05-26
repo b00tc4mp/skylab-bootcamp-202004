2665feb37d297fa65f2be44743496ddcd9fed6f1
manuelbarzi <manuelbarzi@gmail.com>
2020-05-26 12:54:44 +0200

    boost misc-app with official middlewares

---
+++ staff/manuel-barzi/node/misc-app/9/logic/add-sticky.js
@@ -0,0 +1,7 @@
+module.exports = (userId, note, callback) => {
+    // TODO validate input fields
+
+    // TODO check user exists, otherwise error
+
+    // TODO persist sticky on disk (id, note, user)
+}
