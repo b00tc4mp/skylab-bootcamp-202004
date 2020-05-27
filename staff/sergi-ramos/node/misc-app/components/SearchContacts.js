2428e655b40a1397c7a2a349756cceea23e8963e
mac <mac@MacBook-Pro-de-mac.local>
2020-05-27 12:54:29 +0200

    misc-app done

--- staff/sergi-ramos/node/misc-app/components/SearchContacts.js
+++
@@ -1,8 +0,0 @@
-module.exports = query => {
-    return `<section class="search-contacts">
-    <form action="/search-contacts" method="GET">
-        <input name="q" placeholder="search" ${query ? `value="${query}"` : ''}>
-        <button>Search</button>
-    </form>
-</section>`
-}
