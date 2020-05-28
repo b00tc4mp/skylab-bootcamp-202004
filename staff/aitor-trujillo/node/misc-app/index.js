3b94541077db0fa61ffad6118b167d5824fbee74
Aitor1111 <lonesniper11@gmail.com>
2020-05-28 07:55:20 +0200

    boost misc app

--- staff/aitor-trujillo/node/misc-app/index.js
+++ staff/aitor-trujillo/node/misc-app/index.js
@@ -1,27 +1,65 @@
+// TODO Implement pug
+//      Change to res.render
+//      Add all middlewares
+//      
+
 const express = require('express')
 const App = require('./components/App')
-const {retrieveUser,registerUser,authenticateUser,listContacts,addContact,searchContacts,addSticky,searchStickies,listStickies,removeContact,removeSticky}=require('./logic')
-const {Landing,Home,Register,Login,ListContacts,ListStickies,AddContact,AddSticky,SearchContacts,SearchStickies}=require ('./components')
+const { retrieveUser, registerUser, authenticateUser, listContacts, addContact, searchContacts, addSticky, searchStickies, listStickies, removeContact, removeSticky } = require('./logic')
+const { Landing, Home, Register, Login, ListContacts, ListStickies, AddContact, AddSticky, SearchContacts, SearchStickies } = require('./components')
+
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const FileStore = require('session-file-store')(session)
+const path = require('path')
+
+// app.set('view engine', 'pug')
+// app.set('views', './components')
+
+const parseBody = bodyParser.urlencoded({ extended: false })
+
+const cookieSession = session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    // cookie: { secure: true } // WARN this does not make it work => RTFM!
+    cookie: {},
+    store: new FileStore({
+        path: path.join(__dirname, 'data', 'sessions')
+    })
+})
 
 const checkCookie = require('./helper/check-cookie')
 const app = express()
 
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-    checkCookie(req, (error, userId) => {
-        if (userId) return res.redirect('/home')
-        return res.send(App(Landing()))
-    })
+app.get('/', cookieSession, (req, res) => {
+    const { session: { cookiesAccepted, userId } } = req
+
+    if (userId) return res.redirect('/home')
+
+    res.send(App(Landing()))
+
+    // checkCookie(req, (error, userId) => {
+    //     if (userId) return res.redirect('/home')
+    //     return res.send(App(Landing()))
+    // })
 })
 
 //========== REGISTER
 
-app.get('/register', (req, res) => {
-    checkCookie(req, (error, userId) => {
-        if (userId) return res.redirect('/home')
-        return res.send(App(Register()))
-    })
+app.get('/register', cookieSession, (req, res) => {
+    const { session: { cookiesAccepted, userId } } = req
+
+    if (userId) return res.redirect('/home')
+
+    res.send(App(Register()))
+
+    // checkCookie(req, (error, userId) => {
+    //     if (userId) return res.redirect('/home')
+    //     return res.send(App(Register()))
+    // })
 })
 
 app.post('/register', (req, res) => {
@@ -43,13 +81,13 @@ app.post('/register', (req, res) => {
 
         const { name, surname, email, password } = user
         debugger
-        try{
+        try {
             registerUser(name, surname, email, password, (error, id) => {
                 if (error) return res.send(App(Register(error.message)))
 
                 res.redirect('/login')
             })
-        }catch({message}){
+        } catch ({ message }) {
             res.send(App(Register(message)))
             return
         }
@@ -66,29 +104,19 @@ app.get('/login', (req, res) => {
     })
 })
 
-app.post('/login', (req, res) => {
-    let body = ''
+app.post('/login', parseBody, cookieSession, (req, res) => {
+    const { email, password } = req.body
 
-    req.on('data', chunk => body += chunk)
 
-    req.on('end', () => {
+    authenticateUser(email, password, (error, userId) => {
+        if (error) throw error // TODO error handling
 
-        const keyValues = body.split('&')
+        const { session } = req
 
-        const credentials = keyValues.reduce((user, keyValue) => {
-            const [key, value] = keyValue.split('=')
+        session.userId = userId
 
-            user[key] = decodeURIComponent(value)
-
-            return user
-        }, {})
-
-        const { email, password } = credentials
-
-        authenticateUser(email, password, (error, userId) => {
-            if (error) throw error // TODO error handling
-
-            res.cookie('userId', userId)
+        session.save(error => {
+            if (error) throw error
 
             res.redirect('/home')
         })
@@ -97,7 +125,9 @@ app.post('/login', (req, res) => {
 
 //=========== HOME
 
-app.get('/home', (req, res) => {
+app.get('/home', cookieSession, (req, res) => {
+    const { session: { cookiesAccepted, userId } } = req
+
     checkCookie(req, (error, userId) => {
         if (error) return res.redirect('/login')
         if (userId) {
@@ -168,7 +198,7 @@ app.get('/list-contacts', (req, res) => {
 })
 
 app.get('/list-contacts/:idContact', (req, res) => {
-    const {idContact} = req.params
+    const { idContact } = req.params
 
     let body = ''
 
@@ -178,11 +208,11 @@ app.get('/list-contacts/:idContact', (req, res) => {
         checkCookie(req, (error, userId) => {
             if (error) return res.redirect('/login')
             if (userId) {
-                removeContact(userId, idContact, (error, feedback)=> {
+                removeContact(userId, idContact, (error, feedback) => {
                     if (error) throw error
                     if (feedback) return res.redirect('/list-contacts')
                     else throw new Error('something went wrong')
-                    
+
                 })
             }
         })
@@ -264,7 +294,7 @@ app.get('/list-stickies', (req, res) => {
 })
 
 app.get('/list-stickies/:idSticky', (req, res) => {
-    const {idSticky} = req.params
+    const { idSticky } = req.params
 
     let body = ''
 
@@ -274,11 +304,11 @@ app.get('/list-stickies/:idSticky', (req, res) => {
         checkCookie(req, (error, userId) => {
             if (error) return res.redirect('/login')
             if (userId) {
-                removeSticky(userId, idSticky, (error, feedback)=> {
+                removeSticky(userId, idSticky, (error, feedback) => {
                     if (error) throw error
                     if (feedback) return res.redirect('/list-stickies')
                     else throw new Error('something went wrong')
-                    
+
                 })
             }
         })
@@ -311,4 +341,4 @@ app.post('/logout', (req, res) => {
     res.redirect('/login')
 })
 
-app.listen(8007, () => console.log('server running'))
+app.listen(8080, () => console.log('server running'))
