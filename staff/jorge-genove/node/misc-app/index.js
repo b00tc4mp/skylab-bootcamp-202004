c167f8d13f79ba9de4348199e46173aa59a494a0
LLorch1984 <jorgegenove@gmail.com>
2020-05-28 09:55:40 +0200

    node

--- staff/jorge-genove/node/misc-app/index.js
+++ staff/jorge-genove/node/misc-app/index.js
@@ -26,6 +26,7 @@ const cookieSession = session({
 app.use(express.static('public'))
 
 app.get("/landing", cookieSession, (req, res) => {
+  
   const { session: { cookiesAccepted, userId } } = req
 
   if (userId) return res.redirect('/home')
@@ -34,14 +35,16 @@ app.get("/landing", cookieSession, (req, res) => {
 });
 
 app.get("/register", cookieSession, (req, res) => {
+  
   const { session: { cookiesAccepted, userId } } = req
 
   if (userId) return res.redirect('/home')
 
   res.render('Register', { cookiesAccepted })
-
 });
+
 app.post("/register", parseBody, (req, res) => {
+  
   const { body: { name, surname, email, password } } = req
 
   register(name, surname, email, password, (error, id) => {
@@ -52,6 +55,7 @@ app.post("/register", parseBody, (req, res) => {
 });
 
 app.get("/login", cookieSession, (req, res) => {
+  
   const { session: { cookiesAccepted, userId } } = req
 
   if (userId) return res.redirect('/home')
@@ -77,39 +81,41 @@ app.post("/login", parseBody, cookieSession, (req, res) => {
 })
 
 app.get("/home", cookieSession, (req, res) => {
-  const {session : { cookiesAccepted, userId} } = req
+  
+  const { session: { cookiesAccepted, userId } } = req
 
-  if(!userId) return res.redirect('/login')
+  if (!userId) return res.redirect('/login')
 
   retrieveUser(userId, (error, user) => {
-    if(error) throw error
+    if (error) throw error
 
-    const {name} = user
+    const { name } = user
 
-    res.render('Home', {cookiesAccepted, name})
+    res.render('Home', { cookiesAccepted, name })
   })
 })
-app.post('/accept-cookies',cookieSession,(req,res) => {
-  const {session} = req
-  
+
+app.post('/accept-cookies', cookieSession, (req, res) => {
+  const { session } = req
+
   session.cookiesAccepted = true
 
   session.save((error) => {
     res.redirect(req.header('referer'))
   })
 })
- 
- app.get("/contacts",cookieSession, (req, res) => {
 
- const {session : {cookiesAccepted, userId}} = req
+app.get("/contacts", cookieSession, (req, res) => {
+  const { session: { cookiesAccepted, userId } } = req
 
   listContacts(userId, (error, results) => {
     if (error) throw error;
-    res.render('ListContacts', {cookiesAccepted, results});
+    res.render('ListContacts', { cookiesAccepted, results });
   });
 });
+
+app.get("/search-contact", parseCookies, (req, res) => {
   
-/*app.get("/search-contact", parseCookies, (req, res) => {
   const { cookies: { userId } } = req
 
   if (!userId) return res.redirect('/login')
@@ -118,95 +124,104 @@ app.post('/accept-cookies',cookieSession,(req,res) => {
 
 
 });
+
 app.post("/search-contact", parseCookies, parseBody, (req, res) => {
+  
   const { cookies: { userId } } = req
+  
   if (!userId) return res.redirect('/login')
 
   const { body: { q } } = req
+  
   searchContacts(userId, q, (error, contacts) => {
     if (error) throw error
+    
     res.send(App(SearchContacts(q) + ListContacts(contacts)))
   })
 })
- */
-app.post("/logout",cookieSession, (req, res) => {
+
+app.post("/logout", cookieSession, (req, res) => {
+  
   const { session } = req
 
   session.destroy(error => {
-    if(error) throw error
-
-  res.redirect("/login");
-});
-app.get("/add-contact",cookieSession, (req, res) => {
-
-  const {session : {cookiesAccepted, userId} } = req
-
-  if(!userId) return res.redirect ('/login')
-
-  res.render('AddContact', {cookiesAccepted})
-
-});
-
-  app.post("/add-contact",parseBody,cookieSession, (req, res) => {
-  const { body : {  name, surname, email, phone, birth, country}} = req
-
-  const contact = { name, surname, email, phone, birth, country}
-
-  const { session : { userId}} =req
-
-  addContact(contact,userId, (error) => {
-    if (error) throw error 
-
-  res.redirect('/home')
-  })
- }) 
-
-
-
-/*app.get("/search-users", parseCookies, (req, res) => {
-  const { cookies: { userId } } = req
-  if (!userId) res.redirect('/login')
-
-  res.send(App(SearchUsers()))
-})
-app.post("/search-users", parseBody, parseCookies, (req, res) => {
-  const { cookies: { userId } } = req
-  if (!userId) return res.redirect('/login')
-
-  const { body: { q } } = req
-  searchUsers(userId, q, (error, users) => {
     if (error) throw error
-    res.render('SearchUsers',{users,cookiesAccepted} 
-  })
-}) */
-/*app.get("/add-stickies", (req, res) => {
-  res.send(App(AddStickies()));
-});
 
-app.post("/add-stickies", (req, res) => {
-  req.on("data", (chunck) => {
-    let stickie = {};
-    const keyValues = chunck.toString().split("&");
-    keyValues.forEach((element) => {
-      const splitStickie = element.split("=");
-      stickie[splitStickie[0]] = splitStickie[1];
+    res.redirect("/login");
+  });
+
+  app.get("/add-contact", cookieSession, (req, res) => {
+
+    const { session: { cookiesAccepted, userId } } = req
+
+    if (!userId) return res.redirect('/login')
+
+    res.render('AddContact', { cookiesAccepted })
+
+  });
+
+  app.post("/add-contact", parseBody, cookieSession, (req, res) => {
+    
+    const { body: { name, surname, email, phone, birth, country } } = req
+    const contact = { name, surname, email, phone, birth, country }
+    const { session: { userId } } = req
+
+    addContact(contact, userId, (error) => {
+      if (error) throw error
+
+      res.redirect('/home')
+    })
+  })
+
+  app.get("/search-users", parseCookies, (req, res) => {
+    const { cookies: { userId } } = req
+    if (!userId) res.redirect('/login')
+
+    res.send(App(SearchUsers()))
+  })
+
+  app.post("/search-users", parseBody, parseCookies, (req, res) => {
+    const { cookies: { userId } } = req
+    if (!userId) return res.redirect('/login')
+
+    const { body: { q } } = req
+    searchUsers(userId, q, (error, users) => {
+      if (error) throw error
+      res.render('SearchUsers', { users, cookiesAccepted }
+      )
+    })
+  })
+
+  app.get("/add-stickies", (req, res) => {
+    res.send(App(AddStickies()));
+  });
+
+  app.post("/add-stickies", (req, res) => {
+    req.on("data", (chunck) => {
+      let stickie = {};
+      const keyValues = chunck.toString().split("&");
+      keyValues.forEach((element) => {
+        const splitStickie = element.split("=");
+        stickie[splitStickie[0]] = splitStickie[1];
+      });
+      console.log(stickie);
+      stickie.comment = stickie.comment.split("+").join(" ");
+      addStickies(stickie, (error, stickieResult) => {
+        if (error) throw error;
+
+        res.end(App(AddStickies()));
+      });
     });
-    console.log(stickie);
-    stickie.comment = stickie.comment.split("+").join(" ");
-    addStickies(stickie, (error, stickieResult) => {
+  });
+
+  app.get("/stickies", (req, res) => {
+    addStickiesList((error, stickies) => {
+      console.log(stickies);
       if (error) throw error;
 
-      res.end(App(AddStickies()));
+      res.send(App(AddStickiesList(stickies)));
     });
   });
-});
-app.get("/stickies", (req, res) => {
-  addStickiesList((error, stickies) => {
-    console.log(stickies);
-    if (error) throw error;
-
-    res.send(App(AddStickiesList(stickies)));
-  });
-}); */
 })
+
 app.listen(8080);
