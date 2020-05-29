35ebb84a21787f3ff46279733577fc7657ae186b
Juan David Om <anvidmen@gmail.com>
2020-05-29 17:39:37 +0200

    change to promise

--- staff/juandavid-omen/misc-api/data/manager.js
+++ staff/juandavid-omen/misc-api/data/manager.js
@@ -8,52 +8,60 @@ const { Files: { deleteFilesByExtensionFromDirectory }, uid } = require('../util
 module.exports = target => {
     function find(filter, callback) { // filter => { name: 'pepito', surname: 'grillo' }
         if (typeof filter !== 'object') throw new TypeError(`${filter} is not an object`)
-        Function.validate(callback)
+        if (arguments.length > 1) Function.validate(callback)
 
-        fs.readdir(path.join(__dirname, target), (error, files) => {
-            if (error) return callback(error)
-
-            files = files.filter(file => path.extname(file) === '.json')
-
-            const results = []
-
-            if (!files.length) return callback(null, results)
-
-            let i = 0;
-
-            (function readFile() {
-                fs.readFile(path.join(path.join(__dirname, target), files[i]), 'utf8', (error, json) => {
-                    if (error) return callback(error)
-
-                    const existing = JSON.parse(json)
-
-                    const keys = Object.keys(filter)
-
-                    let matches = true
-
-                    for (let j = 0; j < keys.length && matches; j++) {
-                        const key = keys[j]
-
-                        const value = filter[key]
-
-                        matches = existing[key] === value
-                    }
-
-                    if (matches) {
-                        results.push(existing)
-                    }
-
-                    if (++i < files.length) return readFile()
-
-                    callback(null, results)
-                })
-            })()
+        const promise = new Promise((resolve, reject) =>  {
+            fs.readdir(path.join(__dirname, target), (error, files) => {
+                if (error) return reject(error)
+    
+                files = files.filter(file => path.extname(file) === '.json')
+    
+                const results = []
+    
+                if (!files.length) return resolve(results)
+    
+                let i = 0;
+    
+                (function readFile() {
+                    fs.readFile(path.join(path.join(__dirname, target), files[i]), 'utf8', (error, json) => {
+                        if (error) return reject(error)
+    
+                        const existing = JSON.parse(json)
+    
+                        const keys = Object.keys(filter)
+    
+                        let matches = true
+    
+                        for (let j = 0; j < keys.length && matches; j++) {
+                            const key = keys[j]
+    
+                            const value = filter[key]
+    
+                            matches = existing[key] === value
+                        }
+    
+                        if (matches) {
+                            results.push(existing)
+                        }
+    
+                        if (++i < files.length) return readFile()
+    
+                        resolve(results)
+                    })
+                })()
+            })
         })
-    }
+        
+        if (arguments.length === 1) return promise
 
+        promise
+            .then(results => callback(null, results))
+            .catch(callback)
+    }
+    // resolve , reject 
     function deleteMany(callback) {
         deleteFilesByExtensionFromDirectory(path.join(__dirname, target), '.json', error => {
-            if (error) return callback(error)
+            if (error) return callback(error) //reject
 
             callback()
         })
