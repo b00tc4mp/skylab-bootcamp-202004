fb93228024435cf7b93306d65eea48f71b30451d
manuelbarzi <manuelbarzi@gmail.com>
2020-05-29 17:22:40 +0200

    clean misc-app

--- staff/manuel-barzi/misc-api/5/data/index.js
+++ staff/manuel-barzi/misc-api/5/data/index.js
@@ -1,7 +1,3 @@
-const buildManager = require('./manager-builder')
-
 module.exports = {
-    contacts: buildManager('contacts'),
-    stickies: buildManager('stickies'),
     mongo: require('./mongo')
 }
--- staff/manuel-barzi/misc-api/5/logic/index.js
+++ staff/manuel-barzi/misc-api/5/logic/index.js
@@ -1,6 +1,5 @@
 module.exports = {
     registerUser: require('./register-user'),
     authenticateUser: require('./authenticate-user'),
-    retrieveUser: require('./retrieve-user'),
-    addContact: require('./add-contact')
+    retrieveUser: require('./retrieve-user')
 }
