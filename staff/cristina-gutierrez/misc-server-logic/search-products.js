802366f2b00d1fbbf317969c5e90da58feff9f0f
crispine <gutierrezales@hotmail.com>
2020-06-02 19:53:18 +0200

    reorganized app

---
+++ staff/cristina-gutierrez/misc-server-logic/search-products.js
@@ -0,0 +1,31 @@
+require('misc-commons/polyfills/string')
+require('misc-commons/polyfills/json')
+require('misc-commons/polyfills/number')
+const { mongo } = require('../data')
+
+module.exports = query => {
+    String.validate.notVoid(query)
+
+    return mongo.connect()
+        .then(connection => {
+            const db = connection.db()
+
+            const products = db.collection('products')
+
+            return products.find({
+                $or: [
+                    { name: new RegExp(query, 'i') },
+                    { description: new RegExp(query, 'i') }
+                ]
+            }).toArray()
+        })
+        .then(products => {
+            products.forEach(product => {
+                product.id = product._id.toString()
+
+                delete product._id
+            })
+
+            return products
+        })
+}
