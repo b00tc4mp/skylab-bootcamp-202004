6c45531b5c52e55ca874201ed45f430a5ce90bbc
manuelbarzi <manuelbarzi@gmail.com>
2020-06-03 10:38:24 +0200

    update misc

--- staff/manuel-barzi/misc-commons/utils/call.js
+++ staff/manuel-barzi/misc-commons/utils/call.js
@@ -2,27 +2,36 @@ const Http = require('./http')
 require('../polyfills/url')
 require('../polyfills/function')
 
-module.exports = (method, url, body, headers, callback) => {
+module.exports = function (method, url, body, headers, callback) {
     Http.validateMethod(method)
     URL.validate(url)
 
-    Function.validate(callback)
+    if (arguments.length > 4)
+        Function.validate(callback)
 
-    const xhr = new XMLHttpRequest()
+    const promise = new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest()
 
-    xhr.open(method, url)
+        xhr.open(method, url)
 
-    if (headers)
-        for (const key in headers)
-            xhr.setRequestHeader(key, headers[key])
+        if (headers)
+            for (const key in headers)
+                xhr.setRequestHeader(key, headers[key])
 
-    xhr.onload = function () {
-        callback(undefined, this.status, this.responseText)
-    }
+        xhr.onload = function () {
+            resolve({ status: this.status, body: this.responseText })
+        }
 
-    xhr.onerror = function () {
-        callback(new Error('network error'))
-    }
+        xhr.onerror = function () {
+            reject(new Error('network error'))
+        }
 
-    xhr.send(body ? body : undefined)
+        xhr.send(body ? body : undefined)
+    })
+
+    if (arguments.length < 5) return promise
+
+    promise
+        .then(({ status, body }) => callback(null, status, body))
+        .catch(callback)
 }
