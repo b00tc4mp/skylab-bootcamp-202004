d1f886b10641a6bec7c4a25598dd6640dcfaf38c
marcgm97 <mguillemas@gmail.com>
2020-06-03 12:24:22 +0200

    react fixes

--- staff/marc-guillemas/misc-server-logic/authenticate-user.js
+++ staff/marc-guillemas/misc-server-logic/authenticate-user.js
@@ -24,31 +24,31 @@ debugger
             }
         })
 
-        require('misc-commons/polyfills/string')
-        const { mongo } = require('misc-data')
-        const { utils: { Email }, errors: { UnexistenceError, CredentialsError } } = require('misc-commons')
-        const bcrypt = require('bcryptjs')
+        // require('misc-commons/polyfills/string')
+        // const { mongo } = require('misc-data')
+        // const { utils: { Email }, errors: { UnexistenceError, CredentialsError } } = require('misc-commons')
+        // const bcrypt = require('bcryptjs')
         
-        module.exports = (email, password) => {
-            String.validate.notVoid(email)
-            Email.validate(email)
-            String.validate.notVoid(password)
+        // module.exports = (email, password) => {
+        //     String.validate.notVoid(email)
+        //     Email.validate(email)
+        //     String.validate.notVoid(password)
         
-            return mongo.connect()
-                .then(connection => {
-                    const users = connection.db().collection('users')
+        //     return mongo.connect()
+        //         .then(connection => {
+        //             const users = connection.db().collection('users')
         
-                    return users.findOne({ email })
-                })
-                .then(user => {
-                    if (!user) throw new UnexistenceError(`user with e-mail ${email} does not exist`)
+        //             return users.findOne({ email })
+        //         })
+        //         .then(user => {
+        //             if (!user) throw new UnexistenceError(`user with e-mail ${email} does not exist`)
         
-                    return bcrypt.compare(password, user.password)
-                        .then(match => {
-                            if (!match) throw new CredentialsError('wrong password')
+        //             return bcrypt.compare(password, user.password)
+        //                 .then(match => {
+        //                     if (!match) throw new CredentialsError('wrong password')
         
-                            return user._id.toString()
-                        })
-                })
-        } 
+        //                     return user._id.toString()
+        //                 })
+        //         })
+        // } 
 }  
