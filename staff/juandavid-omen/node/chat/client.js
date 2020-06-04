c95f88339ef6fec7441b46c8035971d0750946a4
Juan David Om <anvidmen@gmail.com>
2020-06-04 19:08:15 +0200

    created chat folder on node

---
+++ staff/juandavid-omen/node/chat/client.js
@@ -0,0 +1,33 @@
+const net = require('net')
+const readline = require('readline')
+
+const interface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+const socket = net.createConnection({ host: 'localhost', port: 8080 }, () => {
+    askFrom(name => {
+        socket.write(`FROM: ${name}`)
+
+        askToMessage(toMessage => {
+            socket.write(toMessage)
+        })
+    })
+})
+
+function askFrom (callback) { 
+    interface.question('from? ', callback)
+}
+
+function ask (callback) {
+    interface.question(`to: message?`, callback)
+}
+
+socket.on('data', data => {
+    console.log(`${data.toString()}`)
+
+    askToMessage(toMessage => {
+        socket.write(toMessage)
+    })
+})
