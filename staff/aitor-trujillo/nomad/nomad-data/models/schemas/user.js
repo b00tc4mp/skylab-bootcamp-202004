df975b91453664c7b371b49366ae88bb254ab07b
Aitor1111 <lonesniper11@gmail.com>
2020-06-08 09:31:53 +0200

    start project

---
+++ staff/aitor-trujillo/nomad/nomad-data/models/schemas/user.js
@@ -0,0 +1,20 @@
+const { Schema } = require('mongoose')
+
+
+module.exports = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    surname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: []
+    }
+
+})
