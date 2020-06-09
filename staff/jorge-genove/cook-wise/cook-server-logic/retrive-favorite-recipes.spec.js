cbadaf6887788d2b12c1b70501ddb12d93596738
LLorch1984 <jorgegenove@gmail.com>
2020-06-09 22:58:40 +0200

    'recipes-logic'

--- staff/jorge-genove/cook-wise/cook-server-logic/retrive-favorite-recipes.spec.js
+++ staff/jorge-genove/cook-wise/cook-server-logic/retrive-favorite-recipes.spec.js
@@ -2,20 +2,19 @@ require('dotenv').config()
 
 const { env: { TEST_MONGODB_URL: MONGODB_URL } } = process
 
-const searchRecipes = require('./search-recipe')
+const retrieveFavorite = require('./retrieve-favorite')
 const { random } = Math
 const { expect } = require('chai')
 require('cook-wise-commons/polyfills/json')
 const { mongoose, models: { User, Recipes, Ingredients } } = require('cook-wise-data')
 const bcrypt = require('bcryptjs')
-const { DuplicityError, UnexistenceError } = require('cook-wise-commons/errors')
+const {UnexistenceError } = require('cook-wise-commons/errors')
 
 describe("search-recipe", () => {
     let name, surname, email, password, encryptedPassword, userId
     let recipeName, recipeAuthor, description, time, ingredients = [], recipeId;
     let ingridient, ingredientId
     let quantity, ingredientType;
-    let query
     let user
 
     before(async () => {
@@ -32,7 +31,7 @@ describe("search-recipe", () => {
 
         user = await User.create({ name, surname, email, password, encryptedPassword })
         userId = user.id
-
+        
         ingredientName = `ingredientName-${random()}`;
         const newIngredient = await Ingredients.create({ name: ingredientName });
         ingredientId = newIngredient.id;
@@ -51,34 +50,17 @@ describe("search-recipe", () => {
         const recipe = await Recipes.create({ name: recipeName, author: recipeAuthor, description, time, ingredients })
         recipeId = recipe.id
 
-        await User.findByIdAndUpdate(userId, { $addToSet: { recipes: recipe } });
+        await User.findByIdAndUpdate(userId, { $addToSet: { favoriterecipes: recipeId } });
+
+       
 
     })
     afterEach(async () => {
         await Promise.all([User.deleteMany(), Ingredients.deleteMany(), Recipes.deleteMany(), ingredients.pop()]);
-    })
-    it('must find a recipes matching recipe autho with the query', async () => {
-        query = recipeAuthor
+})
 
-        const result = await searchRecipes(query, userId)
-
-        expect(result).to.exist
-        expect(result).to.be.instanceof(Array)
-        expect(result.length).to.be.equal(1)
-
-        const [singleRecipe] = result;
-        expect(singleRecipe.name).to.exist
-        expect(singleRecipe.author).to.exist
-        expect(singleRecipe.time).to.exist
-        expect(singleRecipe.description).to.exist
-        expect(singleRecipe.ingredients).to.exist
-
-    })
-
-    it('must find a recipes matching recipe name with the query', async () => {
-        query = recipeName
-
-        const result = await searchRecipes(query, userId)
+    it('should retrieve all favorite recipes of the user', async () => {
+        const result = await retrieveFavorite(userId)
 
         expect(result).to.exist
         expect(result).to.be.instanceof(Array)
@@ -90,14 +72,13 @@ describe("search-recipe", () => {
         expect(singleRecipe.time).to.exist
         expect(singleRecipe.description).to.exist
         expect(singleRecipe.ingredients).to.exist
-
     })
 
     it("shold throw an error if not match a user", async () => {
         await User.deleteMany()
         let _error;
         try {
-            await searchRecipes(query,userId)
+            await retrieveFavorite(userId)
         }catch(error) {
             _error = error;
         }
@@ -107,58 +88,21 @@ describe("search-recipe", () => {
         expect(_error.message).to.equal(`user with id ${userId} does not exist`); 
     })
 
-    it("shold throw an error if recipes not be found", async () => {
-        query = 'hola'
-        let _error;
-        try {
-            await searchRecipes(query,userId)
-        }catch(error) {
-            _error = error;
-        }
-
-        expect(_error).to.exist;
-        expect(_error).to.be.instanceof(UnexistenceError);
-        expect(_error.message).to.equal(`${query} is not found like recipe or author`); 
-    })
-
-    it('should throw an error if query its not an string', () => {
+    it('should throw an error if userId its not an string', () => {
         expect(function () {
-            searchRecipes(query, undefined)
-        }).to.throw(TypeError, 'undefined is not a string')
-
-        expect(function () {
-            searchRecipes(query, 1)
-        }).to.throw(TypeError, '1 is not a string')
-
-        expect(function () {
-            searchRecipes(query, null)
-        }).to.throw(TypeError, 'null is not a string')
-
-        expect(function () {
-            searchRecipes(query, true)
-        }).to.throw(TypeError, 'true is not a string')
-    })
-
-it('should throw an error if userId its not an string', () => {
-        expect(function () {
-            searchRecipes(undefined, userId)
+            retrieveFavorite(undefined, recipeId)
         }).to.throw(TypeError, 'undefined is not a string')
     
         expect(function () {
-            searchRecipes(1, userId)
+            retrieveFavorite(1, recipeId)
         }).to.throw(TypeError, '1 is not a string')
     
         expect(function () {
-            searchRecipes(null, userId)
+            retrieveFavorite(null, recipeId)
         }).to.throw(TypeError, 'null is not a string')
     
         expect(function () {
-            searchRecipes(true, userId)
+            retrieveFavorite(true, recipeId)
         }).to.throw(TypeError, 'true is not a string')
-})    
-   
-    after(async () => {
-        await Promise.all([User.deleteMany(), Ingredients.deleteMany(), Recipes.deleteMany(),]);
-        await mongoose.disconnect();
     })
 })
--- staff/jorge-genove/cook-wise/cook-server-logic/search-recipe.js
+++ staff/jorge-genove/cook-wise/cook-server-logic/search-recipe.js
@@ -1,5 +1,21 @@
+/**
+ * 
+ * must found recipes by query
+ * 
+ * @param query must search by this param
+ * 
+ * @param userId it must find the user owner of the recipes
+ * 
+ * @throws UnexistanceError if user doesn't exist
+ * 
+ * @throws UnexistanceError if recipe doesn't match
+ */
+
+
+
+
 require('cook-wise-commons/polyfills/string')
-const { errors: {  UnexistenceError } } = require('cook-wise-commons')
+const { errors: { UnexistenceError } } = require('cook-wise-commons')
 const { models: { User } } = require('cook-wise-data')
 
 module.exports = (query, userId) => {
@@ -7,16 +23,38 @@ module.exports = (query, userId) => {
     String.validate.notVoid(userId)
 
     return (async () => {
-       
-        const user =  await User.findById(userId)
-        if(!user) throw new UnexistenceError(`user with id ${userId} does not exist`)
-       
-       let recipesFind = user.recipes.filter(recipe => recipe.name === query 
-       || recipe.author === query);
 
-       if (recipesFind.length === 0) throw new UnexistenceError(`${query} is not found like recipe or author`)
-        
-       return recipesFind
-       })()
+        const user = await User.findById(userId)
+            .populate({
+                path: 'recipes',
+                populate: {
+                    path: 'ingredients.ingredient',
+                    model: 'Ingredients'
+                }
+            }).lean()
+        if (!user) throw new UnexistenceError(`user with id ${userId} does not exist`)
+
+        let recipesFind = user.recipes.filter(recipe => recipe.name === query
+            || recipe.author === query);
+
+        if (recipesFind.length === 0) throw new UnexistenceError(`${query} is not found like recipe or author`)
+
+        recipesFind.forEach(recipeFind => {
+            delete recipeFind._id
+            delete recipeFind.__v
+
+            recipeFind.ingredients.forEach(singleIng => {
+
+                delete singleIng._id
+                const name = singleIng.ingredient.name
+
+                singleIng.ingredient = name
+            })
+
+
+        })
+
+        return recipesFind
+    })()
 
 }
