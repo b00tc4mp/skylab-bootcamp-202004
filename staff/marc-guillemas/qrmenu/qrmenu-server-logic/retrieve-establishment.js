70ac11f59b8f87b89a244efa822e7b4664cd7cae
marcgm97 <mguillemas@gmail.com>
2020-06-09 12:04:57 +0200

    retrieve establish server logic

---
+++ staff/marc-guillemas/qrmenu/qrmenu-server-logic/retrieve-establishment.js
@@ -0,0 +1,18 @@
+require('qrmenu-commons/pollyfills/string')
+const { models: {Establishment}, mongoose: {ObjectId} } = require('qrmenu-data')
+const {errors: {UnexistenceError}} = require('qrmenu-commons')
+module.exports = userId => {
+    
+    String.validate.notVoid(userId)
+
+    return (async() => {
+        const user = await Establishment.findOne({_id: ObjectId(userId)}, {__v: 0, dishes: 0, orders: 0, password: 0, staff: 0}).lean()
+        if(!user) throw new UnexistenceError(`user with id ${userId} does not exist`)
+
+        user.id = user._id.toString()
+
+        delete user._id
+
+        return user
+    })()
+}
