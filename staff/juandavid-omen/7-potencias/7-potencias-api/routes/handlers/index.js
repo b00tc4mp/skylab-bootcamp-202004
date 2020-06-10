e8dd14dc17613815255dcf49c7bca8d85f2e1ea3
Juan David Om <anvidmen@gmail.com>
2020-06-10 23:09:11 +0200

    fix names handlers

--- staff/juandavid-omen/7-potencias/7-potencias-api/routes/handlers/index.js
+++ staff/juandavid-omen/7-potencias/7-potencias-api/routes/handlers/index.js
@@ -2,10 +2,10 @@ module.exports = {
   registerUser: require('./register-user'),
   authenticateUser: require('./authenticate-user'),
   retrieveUser: require('./retrieve-user'),
-  deleteUser: require('./deleteUser'),
-  createClass: require('./createClass'),
-  searchClass: require('./searchClass'),
-  updateCart: require('./updateCart'),
-  deleteCart: require('./deleteCart'),
-  placeOrder: require('./placeOrder')
+  deleteUser: require('./delete-user'),
+  createClass: require('./create-class'),
+  searchClass: require('./search-class'),
+  updateCart: require('./update-cart'),
+  deleteCart: require('./delete-cart'),
+  placeOrder: require('./place-order')
 }
