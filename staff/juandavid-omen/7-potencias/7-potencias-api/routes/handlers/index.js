1bfe585970afdf267fc0506d5a7a0764612eaf4e
Juan David Om <anvidmen@gmail.com>
2020-06-10 18:43:02 +0200

    added endpoints to index file

---
+++ staff/juandavid-omen/7-potencias/7-potencias-api/routes/handlers/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  registerUser: require('./register-user'),
+  authenticateUser: require('./authenticate-user'),
+  retrieveUser: require('./retrieve-user'),
+  deleteUser: require('./deleteUser'),
+  createClass: require('./createClass'),
+  searchClass: require('./searchClass'),
+  updateCart: require('./updateCart'),
+  deleteCart: require('./deleteCart'),
+  placeOrder: require('./placeOrder')
+}
