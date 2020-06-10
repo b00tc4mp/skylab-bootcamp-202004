bc38214415393859e9d2b1d7ccb0114474707a51
jordigraup <jordigptab@gmail.com>
2020-06-10 16:37:58 +0200

    register user spec

--- staff/jordi-grau/plates/plates-server-logic/register-user.spec.js
+++ staff/jordi-grau/plates/plates-server-logic/register-user.spec.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const { env: { TEST_MONGODB_URL: MONGODB_URL }} = process;
-const { DuplicityError, UnexistenceError } = require('plates-commons/errors');
+const { DuplicityError, UnexistenceError, VoidError } = require('plates-commons/errors');
 const bcrypt = require('bcryptjs');
 const { floor, random } = Math;
 const {expect} = require('chai');
@@ -42,6 +42,32 @@ describe("registerUser", () => {
 
         const isPasswordValid = await bcrypt.compare(password, user.password);
         expect(isPasswordValid).to.be.true;
+    })
+
+    it('should fail to register a new user with wrong data', () => {
+         name = ''
+         
+        expect(() => registerUser(name,surname,email, password)).to.throw(VoidError, `string is empty or blank`)
+
+        name = 'pepito'
+        email = '0.patatero'
+
+        expect(() => registerUser(name,surname,email, password)).to.throw(Error, `${email} is not an e-mail`)
+        
+        name = `name-${random()}`;
+        email = `email-${random()}@gmail.com`;
+        password = '';
+
+        expect(() => registerUser(name,surname,email, password)).to.throw(Error, `string is empty or blank`)
+
+        name = `name-${random()}`;
+        surname = '';
+        email = `email-${random()}@gmail.com`;
+        password = `password-${random()}`;
+
+        expect(() => registerUser(name,surname,email, password)).to.throw(Error, `string is empty or blank`)
+
+
     })
 
     afterEach(async() => {
