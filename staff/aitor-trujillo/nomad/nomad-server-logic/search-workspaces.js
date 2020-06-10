5eb945611f9ed56b0fcbdb30553285c06be0cd8d
Aitor1111 <lonesniper11@gmail.com>
2020-06-10 14:52:38 +0200

    start native & fix search logic

--- staff/aitor-trujillo/nomad/nomad-server-logic/search-workspaces.js
+++ staff/aitor-trujillo/nomad/nomad-server-logic/search-workspaces.js
@@ -6,10 +6,17 @@ module.exports = async (query) => {
 
     String.validate.notVoid(query)
 
-    const workspaces = await Workspace.find({ $or: [{ address: { city: { $regex: `.*${query}.*` } } }, /*{ country: { $regex: `.*${query}.*` } },*/ { name: { $regex: `.*${query}.*` } }] }).lean();
+    const workspaces = await Workspace.find({
+        $or: [
+            { 'address.city': { $regex: `.*${query}.*` } },
+            { 'address.country': { $regex: `.*${query}.*` } },
+            { name: { $regex: `.*${query}.*` } }, { phone: { $regex: `.*${query}.*` } }
+        ]
+    }).lean();
     // const workspaces = await Workspace.find({ name: { $regex: `.*${query}.*` } }).lean();
 
     if (!workspaces.length) throw new Error(`no matchings for "${query}"`)
 
     return workspaces
 }
+
