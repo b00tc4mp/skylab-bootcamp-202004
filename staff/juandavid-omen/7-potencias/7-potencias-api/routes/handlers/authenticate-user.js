b4aab267ee40143972e5a2625dcfa00cf8c9abec
Juan David Om <anvidmen@gmail.com>
2020-06-10 18:27:13 +0200

    added authenticateUser file to handlers-routes

---
+++ staff/juandavid-omen/7-potencias/7-potencias-api/routes/handlers/authenticate-user.js
@@ -0,0 +1,18 @@
+const { env: { SECRET } } = process
+
+const { authenticateUser } = require('7-potencias-server-logic')
+const { handleError } = require('../../helpers')
+const { utils: { jwtPromised } } = require('7-potencias-commons')
+
+module.exports = (req, res) => {
+  const { body: { email, password } } = req
+
+  try {
+    authenticateUser(email, password)
+      .then(userId => jwtPromised.sign({ sub: userId }, SECRET, { expiresIn: '1d' }))
+      .then(token => res.send({ token }))
+      .catch(error => handleError(error, res))
+  } catch (error) {
+    handleError(error, res)
+  }
+}
