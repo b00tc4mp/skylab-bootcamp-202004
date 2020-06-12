c5ccd0e9aeb494aad79f15e1fbd2a48c159bc2ba
alejandromc23 <alejandro.mascort17@hotmail.com>
2020-06-12 13:08:13 +0200

    add retrieves client side

--- staff/alejandro-mascort/escape-me/escape-me-client-logic/retrieve-escape-rooms.js
+++ staff/alejandro-mascort/escape-me/escape-me-client-logic/retrieve-escape-rooms.js
@@ -2,11 +2,13 @@ require('escape-me-commons/polyfills/string')
 const { utils: { call } } = require('escape-me-commons')
 const context = require('./context')
 
-module.exports = function (token, userId) {
+module.exports = function (token, tag, userId) {
     String.validate.notVoid(token)
+    String.validate.notVoid(tag)
+
     if (userId) String.validate.notVoid(userId)
-    debugger
-    return call('GET', `${this.API_URL}/users/${userId ? userId : ''}`,
+
+    return call('GET', `${this.API_URL}/users/escape/${tag}/${userId ? userId : ''}`,
         undefined,
         { 'Authorization': `Bearer ${token}` })
         .then(({ status, body }) => {
