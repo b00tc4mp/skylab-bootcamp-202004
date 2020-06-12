ee745aad3c397758cf4945f6df2f4980c39ba1bd
Aitor1111 <lonesniper11@gmail.com>
2020-06-12 12:31:06 +0200

    fix server logics

--- staff/aitor-trujillo/nomad/nomad-server-logic/index.js
+++ staff/aitor-trujillo/nomad/nomad-server-logic/index.js
@@ -3,5 +3,10 @@ module.exports = {
     authenticateUser: require('./authenticate-user'),
     retrieveUser: require('./retrieve-user'),
     createWorkspace: require('./create-workspace'),
-    retrieveWorkspaceById: require('./retrieve-workspace-by-id')
+    retrieveWorkspaceById: require('./retrieve-workspace-by-id'),
+    retrieveByLocation: require('./retrieve-by-location'),
+    searchWorkspaces: require('./search-workspaces'),
+    addToFavorites: require('./add-to-favorites'),
+    retrieveFavorites: require('./retrieve-favorites'),
+    searchFavorites: require('./search-favorites'),
 }
