e6307a4133c897983ac25ca3956ee52120cf4ff8
Juan David Om <anvidmen@gmail.com>
2020-06-13 19:11:54 +0200

    Add more parameters to create product

--- staff/juandavid-omen/7-potencias/7-potencias-api/routes/handlers/create-product.js
+++ staff/juandavid-omen/7-potencias/7-potencias-api/routes/handlers/create-product.js
@@ -2,10 +2,10 @@ const { createProduct } = require('7-potencias-server-logic')
 const { handleError } = require('../../helpers')
 
 module.exports = (req, res) => {
-  const { body: { name, price, danceStyle } } = req
+  const { body: { name, price, danceStyle, hour, minute } } = req
 
   try {
-    createProduct(name, price, danceStyle)
+    createProduct(name, price, danceStyle, hour, minute)
       .then(() => res.status(201).send())
       .catch(error => handleError(error, res))
   } catch (error) {
--- staff/juandavid-omen/7-potencias/7-potencias-server-logic/create-product.js
+++ staff/juandavid-omen/7-potencias/7-potencias-server-logic/create-product.js
@@ -3,15 +3,15 @@ require('7-potencias-commons/polyfills/number')
 const { models: { Product } } = require('7-potencias-data')
 const { errors: { DuplicityError } } = require('7-potencias-commons')
 
-module.exports = (name, price, danceStyle) => {
+module.exports = (name, price, danceStyle, hour, minute) => {
   String.validate.notVoid(name)
   Number.validate(price)
   String.validate.notVoid(danceStyle)
 
   return Product.findOne({ name })
-    .then(Product => {
-      if (Product) throw new DuplicityError(`Product with name ${name} already exits`)
+    .then(product => {
+      if (product) throw new DuplicityError(`Product with name ${name} already exits`)
 
-      return Product.insertOne({ name, price, danceStyle })
+      return Product.create({ name, price, danceStyle, hour, minute })
     })
 }
