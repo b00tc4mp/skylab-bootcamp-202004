4c3f31d6dcfefb8159d0bdbb4b41955a2ac6bb54
Juan David Om <anvidmen@gmail.com>
2020-06-13 23:41:26 +0200

    Changed parameter name

--- staff/juandavid-omen/7-potencias/7-potencias-api/routes/handlers/update-cart.js
+++ staff/juandavid-omen/7-potencias/7-potencias-api/routes/handlers/update-cart.js
@@ -3,11 +3,11 @@ const { handleError } = require('../../helpers')
 
 module.exports = (req, res) => {
   try {
-    const { body: { classId, amount } } = req
+    const { body: { productId, amount } } = req
 
     const { payload: { sub: userId } } = req
 
-    updateCart(userId, classId, parseInt(amount))
+    updateCart(userId, productId, parseInt(amount))
       .then(() => res.status(200).send())
       .catch(error => handleError(error, res))
   } catch (error) {
