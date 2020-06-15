10c0873a461766fab88b4a98a7617c91644e5618
Juan David Om <anvidmen@gmail.com>
2020-06-15 09:25:59 +0200

    added spec register user

--- staff/juandavid-omen/7-potencias/7-potencias-server-logic/register-user.js
+++ staff/juandavid-omen/7-potencias/7-potencias-server-logic/register-user.js
@@ -15,7 +15,7 @@ module.exports = (name, surname, email, password) => {
   return (async () => {
     const user = await User.findOne({ email })
 
-    if (user) throw new DuplicityError(`user with e-mail ${email} already exists `)
+    if (user) throw new DuplicityError(`user with e-mail ${email} already exists`)
 
     const hash = await bcrypt.hash(password, 8)
 
