55441ce659d1018388699675c6edfde3f50f470e
60225198 <daniel_zamora27@me.com>
2020-06-16 12:37:38 +0200

    dnd init

--- staff/daniel-zamora/moove-it/moove-it-app/src/components/PlaneBuilder.jsx
+++ staff/daniel-zamora/moove-it/moove-it-app/src/components/PlaneBuilder.jsx
@@ -1,15 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './PlaneBuilder.css'
+import './items.css'
+// const background = require('../images/blueprint-background.jpg')
 
+const items = [
+    'double-bed',
+    'single-bed'
+]
 export default function PlaneBuilder () {
+    const [placedItems, setPlacedItems] = useState([])
 
-    return <div className='plane-builder'>
-            <div className="plane-builder__plane" 
-                style={{
-                    width: 32 * 20 + 1,
-                    height: 32 * 18 + 1
-            }}></div>
-            <div className="plane-builder__item"></div>
+    const handleDrag = (e) => {
+        e.dataTransfer.setData("text", e.target.id); 
+    }
 
-    </div>
+    const handleDrop = (e) => {
+        e.preventDefault()
+        const item = e.dataTransfer.getData("text").split("_")[0]
+        placeItem(item, e.clientX, e.clientY)
+    }
+
+    const placeItem = (item, x, y)=> {
+        setPlacedItems(prevState => [...prevState, {item, x, y}])
+    // console.dir({item, x, y})
+    }
+
+    return (
+        <div className="map-builder">
+            <div className="map" 
+            onDragOver={e => e.preventDefault()}
+            onDrop={handleDrop}
+        >
+            {placedItems.map(placed => {
+            return <div className={`placed ${placed.item}`} 
+                    style={{left: placed.x, top:placed.y}}>
+
+            </div>})}
+        </div>
+            <div className="items">{
+                items.map((item, i) => (
+                    <div className={item} 
+                    draggable={true}
+                    onDragStart={handleDrag}
+                    id={`${item}_${i}`}/>))}
+            </div>
+        </div>
+    )
 }
