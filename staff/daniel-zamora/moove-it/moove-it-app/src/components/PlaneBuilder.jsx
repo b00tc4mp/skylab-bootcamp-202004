56e070fffb81ab380e4e87ac8332c4b841b2f9ff
60225198 <daniel_zamora27@me.com>
2020-06-16 17:42:18 +0200

    react plane builder boost; git push

--- staff/daniel-zamora/moove-it/moove-it-app/src/components/PlaneBuilder.jsx
+++ staff/daniel-zamora/moove-it/moove-it-app/src/components/PlaneBuilder.jsx
@@ -7,33 +7,49 @@ const items = [
     'double-bed',
     'single-bed'
 ]
-export default function PlaneBuilder () {
+export default function PlaneBuilder () { debugger
+
     const [placedItems, setPlacedItems] = useState([])
 
     const handleDrag = (e) => {
         e.dataTransfer.setData("text", e.target.id); 
     }
-
+    
+    let item
     const handleDrop = (e) => {
         e.preventDefault()
-        const item = e.dataTransfer.getData("text").split("_")[0]
-        placeItem(item, e.clientX, e.clientY)
+        item = e.dataTransfer.getData("text").split("_")[0]
+        if(!e.target.dataset.placed) {
+            placeItem(item, e.clientX, e.clientY)
+        } else {
+            updateItem(item, e.clientX, e.clientY)
+        }
     }
 
-    const placeItem = (item, x, y)=> {
-        setPlacedItems(prevState => [...prevState, {item, x, y}])
-    // console.dir({item, x, y})
+    const updateItem = (item, x, y) => {
+        
+        setPlacedItems([{item, x, y}])
     }
+    
+    const placeItem = (item, x, y)=> {
+        setPlacedItems(prevPlacedItems =>  ([...prevPlacedItems, {item, x, y}])
+        )}
 
     return (
         <div className="map-builder">
             <div className="map" 
             onDragOver={e => e.preventDefault()}
             onDrop={handleDrop}
-        >
-            {placedItems.map(placed => {
-            return <div className={`placed ${placed.item}`} 
-                    style={{left: placed.x, top:placed.y}}>
+            >
+            {placedItems.map((placed, i) => {
+                if(placed.item !== item)
+                return <div
+                    data-placed = {true}
+                    className={`placed ${placed.item}`} 
+                    style={{left: placed.x, top:placed.y}}
+                    draggable={true}
+                    onDragStart={handleDrag}
+                    id={`${placed.item}_${i}`}>
 
             </div>})}
         </div>
