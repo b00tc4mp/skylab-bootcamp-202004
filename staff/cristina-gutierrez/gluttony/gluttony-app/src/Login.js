315086db40437cdd72f2ecd6bd86d38b945a4890
crispine <gutierrezales@hotmail.com>
2020-06-16 16:27:55 +0200

    touchable tests

--- staff/cristina-gutierrez/gluttony/gluttony-app/src/Login.js
+++ staff/cristina-gutierrez/gluttony/gluttony-app/src/Login.js
@@ -5,6 +5,7 @@ import {
   Image,
   TouchableOpacity,
   View,
+  TouchableWithoutFeedback,
   TextInput
 } from "react-native";
 import { authenticateUser } from "../gluttony-client-logic"
@@ -14,38 +15,40 @@ const Login = props => {
     const [password, setPassword] = useState("");
 
     return (
-        <View style={styles.modalView}>
-            <Text style={styles.modalText}>Inicia sesión para ver más</Text>
-            <Image source={ require('../assets/images/logo-without-color-version.png')} style={styles.logo}/>
-            <TextInput
-                placeholder="email"
-                value={email}
-                style={styles.input}
-                autoCapitalize='none'
-                onChangeText={(text) => setEmail(text)}
-            />
-            <TextInput
-                secureTextEntry={true}
-                placeholder="password"
-                value={password}
-                style={styles.input}
-                onChangeText={(text) => setPassword(text)}
-            />
+        <TouchableWithoutFeedback>
+            <View style={styles.modalView}>
+                <Text style={styles.modalText}>Inicia sesión para ver más</Text>
+                <Image source={ require('../assets/images/logo-without-color-version.png')} style={styles.logo}/>
+                <TextInput
+                    placeholder="email"
+                    value={email}
+                    style={styles.input}
+                    autoCapitalize='none'
+                    onChangeText={(text) => setEmail(text)}
+                />
+                <TextInput
+                    secureTextEntry={true}
+                    placeholder="password"
+                    value={password}
+                    style={styles.input}
+                    onChangeText={(text) => setPassword(text)}
+                />
 
-            <TouchableOpacity style={styles.openButton} onPress={() => authenticateUser(email, password)}>
-                <Text style={styles.textStyle}>Iniciar sesión</Text>
-            </TouchableOpacity>
-            <Text style={{ ...styles.modalText, marginBottom: 8 }}>o</Text>
-            <TouchableOpacity style={styles.openButton} onPress={props.onGoToRegister}>
-                <Text style={styles.textStyle}>Regístrate</Text>
-            </TouchableOpacity>
+                <TouchableOpacity style={styles.openButton} onPress={() => authenticateUser(email, password)}>
+                    <Text style={styles.textStyle}>Iniciar sesión</Text>
+                </TouchableOpacity>
+                <Text style={{ ...styles.modalText, marginBottom: 8 }}>o</Text>
+                <TouchableOpacity style={styles.openButton} onPress={props.onGoToRegister}>
+                    <Text style={styles.textStyle}>Regístrate</Text>
+                </TouchableOpacity>
 
-            <Text 
-                style={{ ...styles.modalText, marginTop: 20, textDecorationLine: "underline", fontWeight: "300" }} 
-                onPress={props.onCloseModal}>
-                Mejor en otro momento
-            </Text>
-        </View>
+                <Text 
+                    style={{ ...styles.modalText, marginTop: 20, textDecorationLine: "underline", fontWeight: "300" }} 
+                    onPress={props.onCloseModal}>
+                    Mejor en otro momento
+                </Text>
+            </View>
+        </TouchableWithoutFeedback>
     )
 }
 
--- staff/cristina-gutierrez/gluttony/gluttony-app/src/Modal.js
+++ staff/cristina-gutierrez/gluttony/gluttony-app/src/Modal.js
@@ -5,6 +5,7 @@ import {
   StyleSheet,
   Text,
   TouchableOpacity,
+  TouchableWithoutFeedback,
   View,
 } from "react-native";
 import Login from "./Login";
@@ -15,36 +16,38 @@ const ModalAuthentication = () => {
         const [view, setView] = useState("login");
 
         return (
-            <View style={styles.centeredView}>
-                <Modal
-                    animationType="slide"
-                    transparent={true}
-                    visible={modalVisible}
-                    onRequestClose={() => {
-                    Alert.alert("Modal has been closed.");
-                    }}
-                >
-                    <View style={styles.centeredView}>
-                        { view === "login" ? <Login 
-                            onGoToRegister={ () => setView("register") } 
-                            onCloseModal={() => setModalVisible(false) }
-                        /> : <Text /> }
-                        { view === "register" ? <Register 
-                            onGoToLogin={ () => setView("login") } 
-                            onCloseModal={() => setModalVisible(false) }
-                        /> : <Text /> }
-                    </View>
-                </Modal>
+            <TouchableWithoutFeedback>
+                <View style={styles.centeredView}>
+                    <Modal
+                        animationType="slide"
+                        transparent={true}
+                        visible={modalVisible}
+                        onRequestClose={() => {
+                        Alert.alert("Modal has been closed.");
+                        }}
+                    >
+                        <View style={styles.centeredView}>
+                            { view === "login" ? <Login 
+                                onGoToRegister={ () => setView("register") } 
+                                onCloseModal={() => setModalVisible(false) }
+                            /> : <Text /> }
+                            { view === "register" ? <Register 
+                                onGoToLogin={ () => setView("login") } 
+                                onCloseModal={() => setModalVisible(false) }
+                            /> : <Text /> }
+                        </View>
+                    </Modal>
 
-                <TouchableOpacity
-                    style={styles.openButton}
-                    onPress={() => {
-                    setModalVisible(true);
-                    }}
-                >
-                    <Text style={styles.textStyle}>Accede a Gluttony</Text>
-                </TouchableOpacity>
-            </View>
+                    <TouchableOpacity
+                        style={styles.openButton}
+                        onPress={() => {
+                        setModalVisible(true);
+                        }}
+                    >
+                        <Text style={styles.textStyle}>Accede a Gluttony</Text>
+                    </TouchableOpacity>
+                </View>
+            </TouchableWithoutFeedback>
         )
 }
 
