cd68b2084096363aaa3071d3dc0c21ff7d2a8150
Juan David Om <anvidmen@gmail.com>
2020-06-17 11:55:58 +0200

    added components basics

---
+++ staff/juandavid-omen/7-potencias/7-potencias-app/src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './NavBar.sass'
+
+export default function ({ children }) {
+  return (
+    <nav classNameName='navbar'>
+
+      {children}
+
+    </nav>
+  )
+}
