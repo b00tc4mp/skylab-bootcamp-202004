c97fc98124e5e79bf3966a7f5fdd01aa07653129
Juan David Om <anvidmen@gmail.com>
2020-06-18 15:40:30 +0200

    modified spec update cart

--- staff/juandavid-omen/7-potencias/7-potencias-server-logic/update-cart.spec.js
+++ staff/juandavid-omen/7-potencias/7-potencias-server-logic/update-cart.spec.js
@@ -1,3 +1,5 @@
+/* eslint-env mocha */
+
 require('dotenv').config()
 
 const { env: { TEST_MONGODB_URL: MONGODB_URL } } = process
@@ -7,43 +9,46 @@ const { expect } = require('chai')
 require('7-potencias-commons/polyfills/json')
 require('7-potencias-commons/polyfills/math')
 const { random, randomIntegerBetween } = Math
-const { mongoose, models: { User, Product } } = require('7-potencias-data')
-const { errors: { UnexistenceError, ValueError } } = require('7-potencias-commons')
+const { mongoose, models: { User, Lesson } } = require('7-potencias-data')
+const { errors: { UnexistenceError } } = require('7-potencias-commons')
 
 describe('logic - update cart', () => {
-  let users, products
-
   before(() => mongoose.connect(MONGODB_URL))
 
-  let userId, productId, quantity
+  let userId, productSelectionId, quantity
 
   beforeEach(async () => {
     await User.deleteMany()
-    await Product.deleteMany()
+    await Lesson.deleteMany()
 
     const user = {
       name: `name-${random()}`,
       surname: `surname-${random()}`,
       email: `e-${random()}@mail.com`,
-      password: `password-${random()}`
+      password: `password-${random()}`,
+      cart: [`cart-${random()}`],
+      orders: [`orders-${random()}`]
     }
 
-    const product = {
+    const lesson = {
       name: `name-${random()}`,
-      description: `description-${random()}`,
       price: random() * 1000,
-      url: `url-${random()}`
+      style: `style-${random()}`,
+      hour: random() * 24,
+      minute: random() * 60,
+      day: random() * 7,
+      month: random() * 12,
+      year: random() * 2000
     }
 
     quantity = randomIntegerBetween(1, 100)
 
     userId = await User.create(user)
-    productId = await Product.create(product)
-
+    lessonId = await Lesson.create(lesson)
   })
 
-  it('should succeed on existing user and product', async () => {
-    const result = await updateCart(userId, productId, quantity)
+  it('should succeed on existing user and lesson', async () => {
+    const result = await updateCart(userId, productSelectionId, quantity)
     expect(result).to.be.undefined
 
     const user = await User.findById(userId)
@@ -54,50 +59,48 @@ describe('logic - update cart', () => {
     expect(cart).to.exist
     expect(cart).to.have.lengthOf(1)
 
-    const [_product] = cart
+    const [_lesson] = cart
 
-    expect(_product).to.exist
+    expect(_lesson).to.exist
 
-    const { product, quantity: _quantity } = _product
+    const { lesson, quantity: _quantity } = _lesson
 
-    expect(product.toString()).to.equal(productId)
+    expect (lesson.toString()).to.equal(productSelectionId)
     expect(_quantity).to.equal(quantity)
   })
 
-  it('should fail on trying to remove a whole product that is not already in cart', async () => {
+  it('should fail on trying to remove a whole lesson that is not already in cart', async () => {
     let error
     try {
-      await updateCart(userId, productId, quantity = 0)
+      await updateCart(userId, productSelectionId, quantity = 0)
     } catch (err) {
       error = err
     }
     expect(error).to.exist
 
     expect(error).to.be.instanceOf(UnexistenceError)
-    expect(error.message).to.equal(`product with id ${productId} does not exist in cart for user with id ${userId}`)
+    expect(error.message).to.equal(`lesson with id ${productSelectionId} does not exist in cart for user with id ${userId}`)
   })
 
   it('should fail on negative quantity', async () => {
     quantity = -1
 
     expect(async () => {
-      await updateCart(userId, productId, quantity)
+      await updateCart(userId, productSelectionId, quantity)
     }).to.throw(TypeError, `${quantity} is not a positive number`)
   })
 
   it('should fail on non-string user id', () => {
-
     expect(async () => {
-      await updateCart(true, productId, quantity)
+      await updateCart(true, productSelectionId, quantity)
     }).to.throw(TypeError, 'true is not a string')
 
     expect(async () => {
-      await updateCart(1, productId, quantity)
+      await updateCart(1, productSelectionId, quantity)
     }).to.throw(TypeError, '1 is not a string')
   })
 
-  it('should fail on non-string product id', () => {
-
+  it('should fail on non-string lesson id', () => {
     expect(async () => {
       await updateCart(userId, true, quantity)
     }).to.throw(TypeError, 'true is not a string')
@@ -105,26 +108,24 @@ describe('logic - update cart', () => {
     expect(async () => {
       await updateCart(userId, 1, quantity)
     }).to.throw(TypeError, '1 is not a string')
-
   })
 
   it('should fail on non-numeric quantity', () => {
     expect(async () => {
-      await updateCart(userId, productId, true)
+      await updateCart(userId, productSelectionId, true)
     }).to.throw(TypeError, 'true is not a number')
 
     expect(async () => {
-      await updateCart(userId, productId, 'abc')
+      await updateCart(userId, productSelectionId, 'abc')
     }).to.throw(TypeError, 'abc is not a number')
 
     expect(async () => {
-      await updateCart(userId, productId, NaN)
+      await updateCart(userId, productSelectionId, NaN)
     }).to.throw(TypeError, 'NaN is not a number')
-
   })
 
   it('should fail when user does not exist', () =>
-    updateCart(userId = ObjectId().toString(), productId, 1)
+    updateCart(userId = ObjectId().toString(), productSelectionId, 1)
       .then(() => { throw new Error('it should not reach this point') })
       .catch(error => {
         expect(error).to.exist
@@ -134,20 +135,20 @@ describe('logic - update cart', () => {
       })
   )
 
-  it('should fail when product does not exist', () =>
-    updateCart(userId, productId = ObjectId().toString(), 1)
+  it('should fail when lesson does not exist', () =>
+    updateCart(userId, productSelectionId = ObjectId().toString(), 1)
       .then(() => { throw new Error('it should not reach this point') })
       .catch(error => {
         expect(error).to.exist
 
         expect(error).to.be.instanceOf(UnexistenceError)
-        expect(error.message).to.equal(`product with id ${productId} does not exist`)
+        expect(error.message).to.equal(`lesson with id ${productSelectionId} does not exist`)
       })
   )
 
   afterEach(async () => {
-   await User.deleteMany()
-   await Product.deleteMany()
+    await User.deleteMany()
+    await Lesson.deleteMany()
   })
 
   after(() => mongoose.disconnect())
