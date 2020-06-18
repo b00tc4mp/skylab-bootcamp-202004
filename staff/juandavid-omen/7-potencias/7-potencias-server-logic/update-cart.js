a5dd0d9b23a9ea53653166d08308b5245ec08ed0
Juan David Om <anvidmen@gmail.com>
2020-06-18 12:29:57 +0200

    modified paramaters update cart server

--- staff/juandavid-omen/7-potencias/7-potencias-server-logic/update-cart.js
+++ staff/juandavid-omen/7-potencias/7-potencias-server-logic/update-cart.js
@@ -1,12 +1,11 @@
 require('7-potencias-commons/polyfills/string')
-require('7-potencias-commons/polyfills/json')
 require('7-potencias-commons/polyfills/number')
 const { errors: { UnexistenceError } } = require('7-potencias-commons')
-const { models: { User, Product } } = require('7-potencias-data')
+const { models: { User, ProductSelection } } = require('7-potencias-data')
 
-module.exports = (userId, productId, quantity) => {
+module.exports = (userId, productSelectionId, quantity) => {
   String.validate.notVoid(userId)
-  String.validate.notVoid(productId)
+  String.validate.notVoid(productSelectionId)
   Number.validate.positive(quantity)
 
   return (async () => {
@@ -14,28 +13,20 @@ module.exports = (userId, productId, quantity) => {
 
     if (!user) throw new UnexistenceError(`user with id ${userId} does not exist`)
 
-    const product = await Product.findById(productId).lean()
+    const productSelection = await ProductSelection.findById(productSelectionId).lean()
 
-    if (!product) throw new UnexistenceError(`product with id ${productId} does not exist`)
+    if (!productSelection) throw new UnexistenceError(`product selection with id ${productSelectionId} does not exist`)
 
     const { cart = [] } = user
 
-    const index = cart.findIndex(item => item.product.toString() === productId)
+    const index = cart.findIndex(item => item._id.toString() === productSelectionId)
 
     if (quantity === 0) {
-      if (index < 0) throw new UnexistenceError(`product with id ${productId} does not exist in cart for user with id ${userId}`)
+      if (index < 0) throw new UnexistenceError(`product selection with id ${productSelectionId} does not exist in cart for user with id ${userId}`)
 
       cart.splice(index, 1)
     } else {
-      let productQuantity
-
-      if (index < 0) {
-        productQuantity = { product: productId }
-
-        cart.push(productQuantity)
-      } else productQuantity = cart[index]
-
-      productQuantity.quantity = quantity
+      cart.push(productSelection)
     }
 
     return user.save({ userId, $set: { cart } })
