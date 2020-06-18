d5da4717e1713efb4cde2c3158281fdcca3f452c
luislourencos <luis.lourencos@gmail.com>
2020-06-18 20:49:48 +0200

    add more components react

---
+++ staff/sergio-luis/misc-app/build/precache-manifest.2cef4d697bfa3a01243f6ad82a33c12f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7226c39e140bdd4dd449c28839543d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d48f5c8d7d536ddba0f",
+    "url": "/static/css/main.cdd90b92.chunk.css"
+  },
+  {
+    "revision": "e84fd4ca8b8a8fa08ef8",
+    "url": "/static/js/2.ae5fc1e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ae5fc1e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d48f5c8d7d536ddba0f",
+    "url": "/static/js/main.d204373f.chunk.js"
+  },
+  {
+    "revision": "3885abc4f1286fb896cc",
+    "url": "/static/js/runtime-main.d0568416.js"
+  }
+]);
