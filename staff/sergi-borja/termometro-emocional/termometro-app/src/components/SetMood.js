cbf222af0a889c02e01be3e12239ac16aa16d32f
sergiborja <sergiborja05@gmail.com>
2020-06-19 20:07:29 +0200

    set-mood compo almost done with cool styles

--- staff/sergi-borja/termometro-emocional/termometro-app/src/components/SetMood.js
+++ staff/sergi-borja/termometro-emocional/termometro-app/src/components/SetMood.js
@@ -6,7 +6,7 @@ const { setMood } = require('termometro-client-logic')
 
 function SetMood({ token }) {
 
-    const [value, setValue] = useState(7)
+    const [value, setValue] = useState('7')
 
     const handleSetMood = (event) => {
 
@@ -22,25 +22,32 @@ function SetMood({ token }) {
     }
 
     const handleOnChange = (event) => {
-        
+
         setValue(event.target.value)
-        
+
     }
 
     return (
         <section className="sliderContainer">
-                <div className='sliderContainer__thermoContainer'>
+            <div className='sliderContainer__whiteContainer'>
+            <div className='sliderContainer__formContainer'>
+                <h1>Qué tal está tu autoestima hoy?</h1>
+            </div>
+            <div className='sliderContainer__thermoContainer'>
                 <img src={thermometer} className='sliderContainer__thermoContainer--thermoImg'></img>
+                <div className={`sliderContainer__thermoContainer--progress ${value === '1' && 'height1'} ${value === '2' && 'height2'} ${value === '3' && 'height3'} ${value === '4' && 'height4'} ${value === '5' && 'height5'} ${value === '6' && 'height6'} ${value === '7' && 'height7'} ${value === '8' && 'height8'} ${value === '9' && 'height9'} ${value === '10' && 'height10'} `}></div>
+                <div className='sliderContainer__value'>{value}</div>
+            </div>
+            <div className='sliderContainer__slider'>
                 <input type="range" min="0" max="10" className="sliderContainer__thermoContainer--slider" value={value} onChange={handleOnChange}></input>
-                <div className={`sliderContainer__thermoContainer--progress ${value==='1' && 'height1'} ${value==='2' && 'height2'} ${value==='3' && 'height3'} ${value==='4' && 'height4'} ${value==='5' && 'height5'} ${value==='6' && 'height6'} ${value==='7' && 'height7'} ${value==='8' && 'height8'} ${value==='9' && 'height9'} ${value==='10' && 'height10'} `}></div>
                 </div>
-                <div>{value}</div>
-            <form onSubmit={handleSetMood}>
-                <button>SUBMIT</button>
-            </form>
+                <form className='sliderContainer__buttonContainer' onSubmit={handleSetMood}>
+                    <button className='sliderContainer__button'>SUBMIT</button>
+                </form>
+        </div>
         </section>
     )
 }
 
 export default SetMood
-{/* <progress min="0" max="10" className="sliderContainer__thermoContainer--progress" value={value} onChange={handleOnChange}></progress> */}
+{/* <progress min="0" max="10" className="sliderContainer__thermoContainer--progress" value={value} onChange={handleOnChange}></progress> */ }
