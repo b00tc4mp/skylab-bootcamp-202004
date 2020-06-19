cc354246b514d6bd72740cf57c5a4c7a4ddfbba9
sergiborja <sergiborja05@gmail.com>
2020-06-19 14:54:29 +0200

    Thermometer responsive and working great!

--- staff/sergi-borja/termometro-emocional/termometro-app/src/components/SetMood.js
+++ staff/sergi-borja/termometro-emocional/termometro-app/src/components/SetMood.js
@@ -22,7 +22,9 @@ function SetMood({ token }) {
     }
 
     const handleOnChange = (event) => {
+        
         setValue(event.target.value)
+        
     }
 
     return (
@@ -30,13 +32,15 @@ function SetMood({ token }) {
                 <div className='sliderContainer__thermoContainer'>
                 <img src={thermometer} className='sliderContainer__thermoContainer--thermoImg'></img>
                 <input type="range" min="0" max="10" className="sliderContainer__thermoContainer--slider" value={value} onChange={handleOnChange}></input>
+                <div className={`sliderContainer__thermoContainer--progress ${value==='1' && 'height1'} ${value==='2' && 'height2'} ${value==='3' && 'height3'} ${value==='4' && 'height4'} ${value==='5' && 'height5'} ${value==='6' && 'height6'} ${value==='7' && 'height7'} ${value==='8' && 'height8'} ${value==='9' && 'height9'} ${value==='10' && 'height10'} `}></div>
                 </div>
-                {/* <div>{value}</div>
+                <div>{value}</div>
             <form onSubmit={handleSetMood}>
                 <button>SUBMIT</button>
-            </form> */}
+            </form>
         </section>
     )
 }
 
 export default SetMood
+{/* <progress min="0" max="10" className="sliderContainer__thermoContainer--progress" value={value} onChange={handleOnChange}></progress> */}
