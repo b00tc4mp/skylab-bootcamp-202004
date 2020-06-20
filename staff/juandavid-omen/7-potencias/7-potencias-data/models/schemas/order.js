f628701df8fca9f7a6c224e384f50e823d1f925f
Juan David Om <anvidmen@gmail.com>
2020-06-20 20:23:07 +0200

    Fix schemas

--- staff/juandavid-omen/7-potencias/7-potencias-data/models/schemas/order.js
+++ staff/juandavid-omen/7-potencias/7-potencias-data/models/schemas/order.js
@@ -2,12 +2,6 @@ const { Schema, SchemaTypes: { ObjectId } } = require('mongoose')
 const productSelection = require('./product-selection')
 
 module.exports = new Schema({
-  user: {
-    type: ObjectId,
-    required: true,
-    ref: 'User'
-  },
-
   productSelections: {
     type: [productSelection],
     required: true
