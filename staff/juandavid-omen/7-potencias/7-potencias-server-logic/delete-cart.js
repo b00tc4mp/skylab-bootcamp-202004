43a660de0fd5c058464aba48bad396f2de959a1b
Juan David Om <anvidmen@gmail.com>
2020-06-21 07:51:33 +0200

    Created spec to delete-cart and modified delete-cart.js

--- staff/juandavid-omen/7-potencias/7-potencias-server-logic/delete-cart.js
+++ staff/juandavid-omen/7-potencias/7-potencias-server-logic/delete-cart.js
@@ -1,5 +1,5 @@
 require('7-potencias-commons/polyfills/string')
-const { errors: UnexistenceError } = require('7-potencias-commons')
+const { errors: { UnexistenceError } } = require('7-potencias-commons')
 const { models: { User } } = require('7-potencias-data')
 module.exports = (userId) => {
   String.validate.notVoid(userId)
