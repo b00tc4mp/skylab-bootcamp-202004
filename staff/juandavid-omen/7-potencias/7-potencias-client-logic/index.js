a53099552f55220dfa301994924f823686d34a70
Juan David Om <anvidmen@gmail.com>
2020-06-22 06:28:39 +0200

    Moidified module exports index

--- staff/juandavid-omen/7-potencias/7-potencias-client-logic/index.js
+++ staff/juandavid-omen/7-potencias/7-potencias-client-logic/index.js
@@ -5,5 +5,8 @@ module.exports = {
   isUserSessionValid: require('./is-user-session-valid'),
   retrieveUser: require('./register-user'),
   isUserLoggedIn: require('./is-user-logged-in'),
-  logoutUser: require('./logout-user')
+  logoutUser: require('./logout-user'),
+  searchLessons: require('./search-lessons'),
+  placeOrder: require('./place-order'),
+  updateCart: require('./update-cart')
 }
