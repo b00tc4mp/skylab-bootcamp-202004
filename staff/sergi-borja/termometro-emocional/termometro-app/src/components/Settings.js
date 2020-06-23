4affb3238f8b7c20cb41f123bcc0862a600e3549
sergiborja <sergiborja05@gmail.com>
2020-06-23 19:27:08 +0200

    styling settings

--- staff/sergi-borja/termometro-emocional/termometro-app/src/components/Settings.js
+++ staff/sergi-borja/termometro-emocional/termometro-app/src/components/Settings.js
@@ -2,27 +2,24 @@ import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 import './Settings.sass'
 
-
-
 function Settings ({rol, handleLogOut}) {
-    
-    // const handleLogOut = () => {
-    //     delete sessionStorage.token
-    //     window.location.reload()
-    // }
 
     return (
         <section className='settingsContainer'>
-            <h1>Ajustes</h1>
-            <br/>
+            <div className='settingsContainer__container'>
+                <h1 className='settingsContainer__title'>Ajustes</h1>
+            </div>
+            <div className='settingsContainer__optionList'>
             <Link className='settingsContainer__option' to='/edit-my-info'>Mis Datos</Link>
             <br/>
             <Link className='settingsContainer__option' >Cambiar Contraseña</Link>
             <br/>
-            {!rol && <Link to='handle-accounts'>Gestionar cuentas</Link>}
-            <br/>
-            <button className='settingsContainer__logOut' onClick={handleLogOut}>Log out</button>
+            {!rol && <Link className='settingsContainer__option' to='handle-accounts'>Gestionar cuentas</Link>}
+            </div>
+            <button className='settingsContainer__option' onClick={handleLogOut}>Log out</button>
         </section>
+
+
     )
 }
 
