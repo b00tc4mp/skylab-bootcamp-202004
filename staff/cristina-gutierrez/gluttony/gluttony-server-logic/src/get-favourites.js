412f5d1249e1d06d09690f330be21ac39569335f
crispine <gutierrezales@hotmail.com>
2020-06-25 02:07:31 +0200

    get favourites test

--- staff/cristina-gutierrez/gluttony/gluttony-server-logic/src/get-favourites.js
+++ staff/cristina-gutierrez/gluttony/gluttony-server-logic/src/get-favourites.js
@@ -1,4 +1,5 @@
 const { models: { Users } } = require("gluttony-data")
+
 /**
  * @param  {string} userId
  */
@@ -7,13 +8,8 @@ module.exports = (userId) => {
 
     return Users.findById(userId, "favouriteStores")
         .populate({ path: 'favouriteStores', options: { lean: true } }).lean()
-        .then(user => {
-            if (!user) throw new Error(`user with id ${userId} does not exist`)
-
-            return user.favouriteStores.map(store => {
-                store.id = store._id
-                
-                return store
-            })
-        })
+        .then(user => user.favouriteStores.map(store => {
+            store.id = store._id
+            return store
+        }))
 }
