559c81e429defdb4cb5b65787f995306e1f69c08
jordigraup <jordigptab@gmail.com>
2020-07-02 18:01:49 +0200

    unset icon from navbar

--- staff/jordi-grau/plates/plates-app/src/components/Navbar.js
+++ staff/jordi-grau/plates/plates-app/src/components/Navbar.js
@@ -4,7 +4,7 @@ import './Navbar.sass'
 
 export default function ({ onGoToHome, onLogout}) {
     return <nav className="navbar">
-        <img src="home-icon.png"  className="navbar__item" onClick={onGoToHome} />
+        <a  className="navbar__item" onClick={onGoToHome}>Home</a>
         <a  className="navbar__item" onClick={onLogout}>Logout</a>        
         </nav>
            
