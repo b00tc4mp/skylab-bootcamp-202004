e34702e1bc89554b98b0f522c3d21f73fbffe86b
Juan David Om <anvidmen@gmail.com>
2020-07-08 17:37:22 +0200

    added and modified components

--- staff/juandavid-omen/7-potencias/7-potencias-app/src/components/CartItems.js
+++ staff/juandavid-omen/7-potencias/7-potencias-app/src/components/CartItems.js
@@ -1,24 +1,39 @@
 import React from 'react'
 
 export default function ({ cart }) {
+  const calculateTotalPrice = cart => {
+    let totalItemsPrice = 0
+
+    for (const productSelection of cart) {
+      const itemPrice = productSelection.product.price * productSelection.quantity
+      totalItemsPrice += itemPrice
+    }
+    return totalItemsPrice
+  }
+
   return (
     <div className='cart-items'>
-      {cart && cart.length ? (cart.map(item => (<>
-        <div className='cart-items__item'>
-          <p className='cart-items__title'>{item.product.name}</p>
-          <div className='cart-items__amount'>
-            <span className='cart-items__qty'>{item.quantity}</span>
-            <span>x</span>
-            <span>${item.product.price}</span>
+      <div className='cart-items__wrapper'>
+        {cart && cart.length ? (cart.map(item => (<>
+          <div className='cart-items__item'>
+            <p className='cart-items__item-title'>{item.product.name}</p>
+            <div className='cart-items__item-amount'>
+              <span className='cart-items__item-qty'>{item.quantity}</span>
+              <span>x</span>
+              <span>${item.product.price}</span>
+            </div>
+            <div className='cart-items__item-total'>
+              <span className='cart-items__item-price'>$</span>
+              <span className='cart-items__item-price'>{item.product.price * item.quantity}</span>
+            </div>
           </div>
-          <div className='cart-items__total'>
-            <span className='cart-items__price'>$</span>
-            <span className='cart-items__price'>{item.product.price * item.quantity}</span>
-          </div>
-        </div>
-      </>))
-      )
-        : (<span className='empty-message'>Your cart is empty</span>)}
+        </>)
+        ))
+          : (<span className='empty-message'>Your cart is empty</span>)}
+      </div>
+      <div className='cart-items__total'>
+        <p>Total</p><p> ${calculateTotalPrice(cart)}</p>
+      </div>
     </div>
   )
 }
