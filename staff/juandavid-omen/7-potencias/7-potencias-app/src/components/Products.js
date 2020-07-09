0a3cd264d2ff85420b0efe240433c65ea6720670
Juan David Om <anvidmen@gmail.com>
2020-07-09 15:41:14 +0200

    added alert to  components

--- staff/juandavid-omen/7-potencias/7-potencias-app/src/components/Products.js
+++ staff/juandavid-omen/7-potencias/7-potencias-app/src/components/Products.js
@@ -2,8 +2,10 @@ import React, { useState, useEffect } from 'react'
 import './Products.sass'
 import Card from './Card'
 import { searchLessons } from '7-potencias-client-logic'
+import { transitions, positions, Provider } from 'react-alert'
+import AlertTemplate from 'react-alert-template-basic'
 
-export default function ({ addToCart }) {
+export default function ({ addToCart, isLoggedIn }) {
   const [error, setError] = useState()
   const [products, setProducts] = useState([])
 
@@ -23,18 +25,27 @@ export default function ({ addToCart }) {
     }
   }, [])
 
+  const options = {
+    timeout: 3000,
+    position: positions.BOTTOM_CENTER,
+    offset: '100px',
+    transition: transitions.SCALE
+  }
+
   return (
     <section className='card'>
-      <section className='miswitch'>
-        <div className='swicht-btn' id='swicht-btn' />
-        <a>Individual</a>
-        <a>Group</a>
-      </section>
-      <section className='description-card'>
-        {products && products.map(product => (<>
-          <Card key={product.id} product={product} addToCart={addToCart} />
-        </>))}
-      </section>
+      <Provider template={AlertTemplate} {...options}>
+        <section className='miswitch'>
+          <div className='swicht-btn' id='swicht-btn' />
+          <a>Individual</a>
+          <a>Group</a>
+        </section>
+        <section className='description-card'>
+          {products && products.map(product => (<>
+            <Card key={product.id} product={product} addToCart={addToCart} isLoggedIn={isLoggedIn} />
+          </>))}
+        </section>
+      </Provider>
     </section>
   )
 }
